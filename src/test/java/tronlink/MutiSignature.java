c7c7739340144a70e07d94c65b2634819c5c2f2b
pangpangfeng <7577288@qq.com>
2019-09-11 11:47:34 +0800

    Add mutisign test case

--- src/test/java/tronlink/MutiSignature.java
+++ src/test/java/tronlink/MutiSignature.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Date;
 
 import common.utils.TronLink;
 import io.appium.java_client.android.AndroidDriver;
@@ -44,8 +45,38 @@ public class MutiSignature {
         //Add permission
         TronLink.testOperation(driver,TronLink.mutliSignAddPermissionId,"click","Enter add permission screen");
         TronLink.testOperation(driver,TronLink.activeScreenMoreId,"click","Show more in add active screen");
+        TronLink.testOperation(driver,TronLink.addPermissionId,"click","Enter add permission");
+        Date date = new Date();
+        String activePermissionName = "activePermission" + date.getTime();
+        TronLink.testOperation(driver,TronLink.permissionNameInputId,"input","Permission_"+activePermissionName,"input permission name");
+        TronLink.testOperation(driver,TronLink.trxTransferOperationInAddPermissionXPath,"click","Add TRX transfer operation");
+        TronLink.testOperation(driver,TronLink.trc10TransferOperationInAddPermissionXPath,"click","Add TRC10 transfer operation");
+        TronLink.testOperation(driver,TronLink.trc20TransferOperationInAddPermissionXPath,"click","Add TRC20 transfer operation");
+        TronLink.testOperation(driver,TronLink.thresholdInAddPermissionInputId,"input",TronLink.activePermissionManager1Address,"Input manager 1 address");
+        TronLink.testOperation(driver,TronLink.permissionWeightInputId,"input",String.valueOf(24),"Input manager 1 weight");
+        TronLink.testOperation(driver,TronLink.addKeyInAddPermissionId,"click","Add key");
+        TronLink.testOperation(driver,TronLink.secondPermissionAddressInputXPath,"input",TronLink.activePermissionManager2Address,"Input manager 2 address");
+        TronLink.testOperation(driver,TronLink.secondPermissionWeightInputXPath,"input",String.valueOf(26),"Input manager 2 weight");
+        TronLink.testOperation(driver,TronLink.addPermissionConfirmId,"click","Confirm the add permission");
+        TronLink.testOperation(driver,TronLink.transactionConfirmInputPasswordId,"input",TronLink.testPassword,"Input password for add permission");
+        TronLink.testOperation(driver,TronLink.transactionConfirmButtonId,"click","Confirm the add permission");
 
 
+        //Modify permission.
+        TronLink.swipeRight(driver);
+        TronLink.swipeRight(driver);
+        TronLink.testOperation(driver,TronLink.activeScreenMoreId,"click","Show the modify and delete icon");
+        TronLink.testOperation(driver,TronLink.modifyPermissionsId,"click","Click modify permissions");
+        Assert.assertTrue(TronLink.isDisplayed(driver,TronLink.permissionOperationId));
+        driver.pressKey(new KeyEvent(AndroidKey.BACK));
+
+        //Delete permission.
+        TronLink.testOperation(driver,TronLink.deletePermissionId,"click","Click delete permissions");
+        TronLink.testOperation(driver,TronLink.deletePermissionCancelId,"click","Cancel delete permissions");
+        TronLink.testOperation(driver,TronLink.deletePermissionId,"click","Click delete permissions");
+        TronLink.testOperation(driver,TronLink.deletePermissionConfirmId,"click","Confirm delete permissions");
+        TronLink.testOperation(driver,TronLink.transactionConfirmInputPasswordId,"input",TronLink.testPassword,"Input password for delete permission");
+        TronLink.testOperation(driver,TronLink.transactionConfirmButtonId,"click","Confirm the delete permission");
     }
 
 
