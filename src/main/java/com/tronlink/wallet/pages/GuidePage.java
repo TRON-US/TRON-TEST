c5197ee84ba7dccaf0b191a0cd5352110e79ba62
kaizhu@tron.network <kaizhu@tron.network>
2019-10-09 16:04:07 +0800

    add Test

---
+++ src/main/java/com/tronlink/wallet/pages/GuidePage.java
@@ -0,0 +1,33 @@
+package com.tronlink.wallet.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import com.tronlink.wallet.UITest.base.Base;
+import io.appium.java_client.android.AndroidDriver;
+
+public class GuidePage extends AbstractPage {
+
+    public AndroidDriver<?> driver;
+
+    public GuidePage(AndroidDriver<?> driver) {
+        super(driver);
+        this.driver = driver;
+    }
+
+    @FindBy(id = "com.tronlink.wallet:id/tv_import")
+    public WebElement impAccount;
+
+
+    public UserAgreementPage enterUserAgreementPage() {
+        try {
+            impAccount.click();
+        }catch (Exception e){
+            new Base().log("impAccount button not found");
+        }
+        return new UserAgreementPage(driver);
+    }
+
+
+
+
+}
---
+++ src/test/java/com/tronklink/wallet/regression/DemoTest.java
@@ -0,0 +1,65 @@
+package com.tronklink.wallet.regression;
+
+import com.tronlink.wallet.UITest.base.Base;
+import com.tronlink.wallet.pages.AssetPage;
+import com.tronlink.wallet.pages.TransferPage;
+import com.tronlink.wallet.pages.VotePage;
+import static org.junit.Assert.assertEquals;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class DemoTest extends Base {
+
+    @BeforeClass
+    public void setUpBeforeClass() throws Exception {
+        setUp();
+        //Base.getSign();
+    }
+
+    @BeforeMethod()
+    public void setUpBefore() throws Exception{
+        DRIVER.closeApp();
+        DRIVER.launchApp();
+        getSign();
+    }
+
+    @AfterClass
+    public void tearDownAfterClass() {
+        //Base.tearDownAfterClass();
+        DRIVER.quit();
+    }
+
+
+    @Test //测试是否正常进行投票页
+    public void test01_checkPopularSearch() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        TransferPage transfer = asset.enterTransferPage();
+        assertEquals(true,transfer.transferTtile_btn.isDisplayed());
+    }
+
+
+    @Test //测试进入投票页
+    public void test02_checkPopular() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        VotePage vote = asset.enterVotePage();
+        assertEquals(true,vote.voteTitle_btn.isDisplayed());
+    }
+
+
+    @Test //测试进入投票页
+    public void test03_checkPopular() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        VotePage vote = asset.enterVotePage();
+        //Assert.assertEquals(vote.voteTitle_btn.getText(),"投票");
+        assertEquals(true,vote.voteTitle_btn.isDisplayed());
+    }
+
+
+
+
+
+}
