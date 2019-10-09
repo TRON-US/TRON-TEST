570c9e1ee1176153779092908b0a050a410f7e49
562389830@qq.com <gx19901002>
2019-10-09 19:24:05 +0800

    修改目录结构及import

--- src/test/java/com/tronklink/wallet/regression/MinePageTest.java
+++ src/test/java/com/tronklink/wallet/regression/MinePageTest.java
@@ -1,21 +1,20 @@
 package com.tronklink.wallet.regression;
 
-
-import com.tronlink.wallet.UITest.base.Base;
-
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+
+import wallet.UITest.base.Base;
 
 /**
  * 我的页面功能测试
  */
 public class MinePageTest extends Base {
 
-    @BeforeClass
-    public void setUpBeforeClass() throws Exception {
-        setUp();
-    }
+//    @BeforeClass
+//    public void setUpBeforeClass() throws Exception {
+//        setUp();
+//    }
 
     @BeforeMethod()
     public void setUpBefore() throws Exception{
