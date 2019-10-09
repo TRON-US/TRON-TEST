7ae5d88e27fed0dd349729bcb0565ee4cebfd5ce
562389830@qq.com <gx19901002>
2019-10-09 19:24:05 +0800

    修改目录结构及import

--- src/test/java/com/tronklink/wallet/regression/AddAssetsTest.java
+++ src/test/java/com/tronklink/wallet/regression/AddAssetsTest.java
@@ -1,20 +1,20 @@
 package com.tronklink.wallet.regression;
 
-import com.tronlink.wallet.UITest.base.Base;
-
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+
+import wallet.UITest.base.Base;
 
 /**
  * 添加资产测试
  */
 public class AddAssetsTest extends Base {
 
-    @BeforeClass
-    public void setUpBeforeClass() throws Exception {
-        setUp();
-    }
+//    @BeforeClass
+//    public void setUpBeforeClass() throws Exception {
+//        setUp();
+//    }
 
     @BeforeMethod()
     public void setUpBefore() throws Exception{
