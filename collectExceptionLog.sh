7827b7b697a2399837b6ed7ce2ff97c9afba7274
pangpangfeng <7577288@qq.com>
2019-10-14 16:00:05 +0800

    Modify shell

--- collectExceptionLog.sh
+++ collectExceptionLog.sh
@@ -4,3 +4,4 @@ ps -ef | grep  "adb logcat" | grep -v grep | awk '{print $2}' | xargs kill -9
 rm -rf exceptionLogs
 rm -rf /Users/tron/Documents/tronlink_task/logs/exceptionLogs
 cp -rf exceptionLogs /Users/tron/Documents/tronlink_task/logs/exceptionLogs
+echo "Finish tronlink task"
