266fe0ffb06aaaee576b5c3f7574730fba2f03c9
562389830@qq.com <gx19901002>
2019-10-17 18:56:39 +0800

    add send trx case

--- src/test/java/wallet/pages/SendTrxSuccessPage.java
+++ src/test/java/wallet/pages/SendTrxSuccessPage.java
@@ -3,6 +3,8 @@ package wallet.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.concurrent.TimeUnit;
+
 import io.appium.java_client.android.AndroidDriver;
 
 public class SendTrxSuccessPage extends AbstractPage {
@@ -16,6 +18,16 @@ public class SendTrxSuccessPage extends AbstractPage {
 
     @FindBy(id = "com.tronlink.wallet:id/tv_count")
     public WebElement trxCount;
+
+
+    @FindBy(id = "com.tronlink.wallet:id/iv_common_left")
+    public WebElement back_btn;
+
+    public AssetPage enterSendTrxPage() throws Exception {
+        back_btn.click();
+        TimeUnit.SECONDS.sleep(1);
+        return new AssetPage(driver);
+    }
 
 
 
