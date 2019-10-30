aa60cc0b56a9971e165f5318e0936163db308872
Aiden <Aiden.Liu@tron.network>
2019-10-30 16:32:20 +0800

    add some new cases

--- src/test/java/com/tronklink/wallet/regression/VoteTest.java
+++ src/test/java/com/tronklink/wallet/regression/VoteTest.java
@@ -12,6 +12,9 @@ import wallet.pages.VoteConfirmPage;
 import wallet.pages.VotePage;
 
 import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * 投票功能测试
  */
@@ -70,6 +73,104 @@ public class VoteTest extends Base {
         Assert.assertEquals(count,"1");
     }
 
+    @Test(description = "enter a number that great than the number of votes available")
+    public void test002_vote01() throws Exception{
+        AssetPage asset = new AssetPage(DRIVER);
+        VotePage vote = asset.enterVotePage();
+
+        TimeUnit.SECONDS.sleep(2);
+        vote.unusualVoteOperate();
+
+        if (vote.reset_btn.getText().equals("Reset")) {
+            String hits = asset.english_availableVote_toast.getText();
+            Assert.assertTrue(hits.contains("Insufficient number of votes available"));
+        }else {
+            String hits = asset.availableVote_toast.getText();
+            Assert.assertTrue(hits.contains("可用投票数不足"));
+        }
+
+
+    }
+
+    @Test(description = "Enter a vote of 0,prompt 'vote number null'")
+    public void test002_vote02() throws Exception{
+        AssetPage asset = new AssetPage(DRIVER);
+        VotePage vote = asset.enterVotePage();
+
+        vote.reset_btn.click();
+        TimeUnit.SECONDS.sleep(1);
+        vote.et_input.sendKeys("0");
+        vote.vote_btn.click();
+        if (vote.reset_btn.getText().equals("Reset")) {
+            String hits = asset.english_availableVote_toast_null.getText();
+            Assert.assertTrue(hits.contains("0 vote"));
+        }else {
+            String hits = asset.availableVote_toast_null.getText();
+            Assert.assertTrue(hits.contains("投票数为空") || hits.contains("0 vote"));
+        }
+
+    }
+
+    @Test(description = "The number of votes entered is empty,prompt 'vote number null'")
+    public void test002_vote03() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        VotePage vote = asset.enterVotePage();
+
+        TimeUnit.SECONDS.sleep(1);
+        vote.reset_btn.click();
+        TimeUnit.SECONDS.sleep(1);
+        vote.vote_btn.click();
+        if (vote.reset_btn.getText().equals("Reset")) {
+            String hits = asset.english_availableVote_toast_null.getText();
+            Assert.assertTrue(hits.contains("0 vote"));
+        }else {
+            String hits = asset.availableVote_toast_null.getText();
+            Assert.assertTrue(hits.contains("投票数为空"));
+        }
+
+    }
+
+    @Test(description = "Freeze before testing")
+    public void test002_aPremise01() throws Exception{
+        AssetPage asset = new AssetPage(DRIVER);
+        asset.enterFrozenAndThawingPage();
+
+        TimeUnit.SECONDS.sleep(2);
+        FrozenAndUnfreezePage frozenAndThawingPage =new FrozenAndUnfreezePage(DRIVER);
+        frozenAndThawingPage.inputFrozenCountAndSure("10");
+    }
+
+    @Test(description = "the premise of vote ")
+    public void test002_aPremise02() throws Exception{
+        AssetPage asset = new AssetPage(DRIVER);
+        VotePage vote = asset.enterVotePage();
+
+        TimeUnit.SECONDS.sleep(2);
+        VoteConfirmPage voteConfirmPage = vote.setrVotePremise();
+        voteConfirmPage.voteOperate();
+    }
+
+    @Test(description = "Gets the address of the second candidate")
+    public void test002_vote04() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        VotePage vote = asset.enterVotePage();
+
+        TimeUnit.SECONDS.sleep(1);
+        vote.checkTheSecondInfoOfVoted();
+        TimeUnit.SECONDS.sleep(3);
+        VotePage.Address_Second = vote.voted_address.get(1).getText();
+    }
+
+    @Test(description = "Search bar input")
+    public void test002_vote05() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        VotePage vote = asset.enterVotePage();
+
+        TimeUnit.SECONDS.sleep(2);
+        vote.checkTheSecondInfoOfVoted01();
+        TimeUnit.SECONDS.sleep(3);
+        Assert.assertTrue(VotePage.getSecondAddress().equals(vote.voted_address.get(0).getText()));
+    }
 
 
 }
