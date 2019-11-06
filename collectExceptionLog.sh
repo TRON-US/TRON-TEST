aadbbf3b09cacc238d7890decb4b7d2d2f384d46
562389830@qq.com <gx19901002>
2019-11-06 15:43:48 +0800

    Update dir

--- collectExceptionLog.sh
+++ collectExceptionLog.sh
@@ -3,4 +3,4 @@ ps -ef | grep  listenTronlinkException.sh | grep -v grep | awk '{print $2}' | xa
 ps -ef | grep  "adb logcat" | grep -v grep | awk '{print $2}' | xargs kill -9
 rm -rf /Users/tron/Documents/tronlink_task/logs/exceptionLogs
 cp -rf exceptionLogs /Users/tron/Documents/tronlink_task/logs/exceptionLogs
-echo "Finish tronlink task"
+echo "Finish android.com.tronlink task"
