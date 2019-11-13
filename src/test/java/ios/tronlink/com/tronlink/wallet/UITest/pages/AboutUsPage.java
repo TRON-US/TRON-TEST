527abab916ee063564a351749ec9202f35dba358
hechuan <hccgk@sina.com>
2019-11-13 18:48:53 +0800

    add ios test case in MinePage logic same to android

---
+++ src/test/java/ios/tronlink/com/tronlink/wallet/UITest/pages/AboutUsPage.java
@@ -0,0 +1,25 @@
+package ios.tronlink.com.tronlink.wallet.UITest.pages;
+
+import io.appium.java_client.ios.IOSDriver;
+
+
+import java.util.concurrent.TimeUnit;
+/**
+ * 关于我们页面
+ */
+public class AboutUsPage extends AbstractPage {
+
+    public IOSDriver<?> driver;
+
+    public AboutUsPage(IOSDriver<?> driver) {
+        super(driver);
+        this.driver = driver;
+    }
+
+
+
+
+
+
+
+}
---
+++ src/test/java/ios/tronlink/com/tronlink/wallet/regression/MineTest.java
@@ -0,0 +1,59 @@
+package ios.tronlink.com.tronlink.wallet.regression;
+
+import ios.tronlink.com.tronlink.wallet.UITest.base.Base;
+import ios.tronlink.com.tronlink.wallet.UITest.pages.AnnouncementPage;
+import ios.tronlink.com.tronlink.wallet.UITest.pages.MinePage;
+import ios.tronlink.com.tronlink.wallet.UITest.pages.AssetPage;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+import java.util.concurrent.TimeUnit;
+
+import ios.tronlink.com.tronlink.wallet.UITest.pages.FriendInvitationPage;
+import ios.tronlink.com.tronlink.wallet.utils.Helper;
+
+public class MineTest extends Base {
+
+    @Parameters({"privateKey"})
+    @BeforeClass(alwaysRun = true)
+    public void setUpBefore(String privateKey) throws Exception {
+        new Helper().getSign(privateKey,DRIVER);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void afterMethod(){
+        DRIVER.closeApp();
+        DRIVER.launchApp();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownAfterClass() {
+        DRIVER.quit();
+    }
+
+
+    @Test(description = "Friend invitation Test",alwaysRun = true)
+    public void test001_friendInvitation() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        MinePage minePage = asset.enterMinePage();
+        TimeUnit.SECONDS.sleep(1);
+        FriendInvitationPage friendInvitation = minePage.enterFriendInvitationPage();
+        Assert.assertTrue(friendInvitation.friendInvitation_title.isDisplayed());
+        TimeUnit.SECONDS.sleep(3);
+
+    }
+
+    @Test(description = "Announcement Test",alwaysRun = true)
+    public void test002_bulletin() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        MinePage minePage = asset.enterMinePage();
+        TimeUnit.SECONDS.sleep(1);
+        AnnouncementPage announcement = minePage.enterAnnouncementPage();
+        TimeUnit.SECONDS.sleep(3);
+
+    }
+
+
+
+
+}
