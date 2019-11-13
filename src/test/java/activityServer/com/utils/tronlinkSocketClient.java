3674eb90bbf19002110cd77ec824465eaeecb173
pangpangfeng <7577288@qq.com>
2019-11-13 14:45:37 +0800

    Update tronlink.apk

---
+++ src/test/java/activityServer/com/utils/tronlinkSocketClient.java
@@ -0,0 +1,42 @@
+package activityServer.com.utils;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Iterator;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+/**
+ * Created by wangzihe on 2019/11/13.
+ */
+
+public class tronlinkSocketClient extends WebSocketClient {
+    public tronlinkSocketClient(String url) throws URISyntaxException {
+        super(new URI(url));
+    }
+
+    @Override
+    public void onOpen(ServerHandshake shake) {
+        System.out.println("握手...");
+        for(Iterator<String> it = shake.iterateHttpFields(); it.hasNext();) {
+            String key = it.next();
+            System.out.println(key+":"+shake.getFieldValue(key));
+        }
+    }
+
+    @Override
+    public void onMessage(String paramString) {
+        System.out.println("接收到消息："+paramString);
+    }
+
+    @Override
+    public void onClose(int paramInt, String paramString, boolean paramBoolean) {
+        System.out.println("关闭...");
+    }
+
+    @Override
+    public void onError(Exception e) {
+        System.out.println("异常"+e);
+
+    }
+
+}
