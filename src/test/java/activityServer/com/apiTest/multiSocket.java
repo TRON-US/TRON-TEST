d21c8d1afeb591dd07700852aeb8a1733d05cc6c
pangpangfeng <7577288@qq.com>
2019-11-13 14:45:37 +0800

    Update tronlink.apk

--- src/test/java/activityServer/com/apiTest/multiSocket.java
+++ src/test/java/activityServer/com/apiTest/multiSocket.java
@@ -1,16 +1,15 @@
 package activityServer.com.apiTest;
 
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-
 import org.apache.http.HttpResponse;
-import org.junit.Assert;
 import org.testng.annotations.Test;
-
+import java.net.URI;
 import java.util.HashMap;
-
+import java.util.Iterator;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
 import activityServer.com.utils.api;
-
+import activityServer.com.utils.tronlinkSocketClient;
 /**
  * Created by wangzihe on 2019/11/12.
  */
@@ -20,24 +19,35 @@ public class multiSocket {
     private JSONObject responseContent;
     private HashMap<String,String> param = new HashMap<>();
 
+
     @Test(enabled = true, description = "Mutli socket test")
-    public void multiTrxRecord() throws Exception {
+    public void multiSocket() throws Exception {
+        //String url = "wss://testlist.tronlink.org/api/wallet/multi/socket?address=TBExF3mNvnhmEFgHW4TmYXXdhevRchnQyb&state=1";
+        String url = "wss://testlist.tronlink.org/api/wallet/multi/socket/";
+        URI uri = new URI(url);
+        tronlinkSocketClient mWs = new tronlinkSocketClient(url);
+
+        mWs.connect();
+
+        while (!mWs.getReadyState().equals(WebSocketClient.READYSTATE.OPEN)) {
+
+            System.out.println(mWs.getURI().toString());
+            System.out.println("还没有打开");
+            System.out.println(mWs.isOpen());
+        }
+        mWs.send("address=TBExF3mNvnhmEFgHW4TmYXXdhevRchnQyb&state=1");
+
+
+
+
+        System.exit(1);
+
         param.put("address","TBExF3mNvnhmEFgHW4TmYXXdhevRchnQyb");
         param.put("start","0");
         param.put("limit","40");
         param.put("state","0");
         param.put("netType","main_net");
-        response = api.multiSocket(param);
-        responseContent = api.parseResponseContent(response);
-        api.printJsonContent(responseContent);
-        Assert.assertTrue(responseContent.size() >= 3);
-        Assert.assertTrue(responseContent.getString("message").equals("OK"));
 
-        JSONArray array = responseContent.getJSONObject("data").getJSONArray("data");
-        for (int i = 0; i < array.size();i++) {
-            System.out.println(array.get(i));
-            System.out.println("-----------------------");
-        }
 
     }
 }
