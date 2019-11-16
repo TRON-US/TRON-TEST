1186870d5cb551e36f598080ce6ec44d2d1de415
wangzichichi <39043115+wangzichichi@users.noreply.github.com>
2019-11-16 12:40:31 +0800

    Revert "Dev auto ioss make  organization"

--- src/test/java/ios/tronlink/com/tronlink/wallet/UITest/pages/TransactionRecordPage.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/UITest/pages/TransactionRecordPage.java
@@ -20,12 +20,12 @@ public class TransactionRecordPage extends AbstractPage {
 
 
 //
-    @FindBy(id = "com.tronlink.wallet:id/tv_three")
-    public WebElement owner_text;
-
-
-    @FindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.HorizontalScrollView/android.widget.LinearLayout/android.support.v7.app.ActionBar.Tab[2]/android.widget.TextView")
-    public WebElement navigation_tab;
+//    @FindBy(id = "com.tronlink.wallet:id/tv_three")
+//    public WebElement owner_text;
+//
+//
+//    @FindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.HorizontalScrollView/android.widget.LinearLayout/android.support.v7.app.ActionBar.Tab[2]/android.widget.TextView")
+//    public WebElement navigation_tab;
 
 
 }
