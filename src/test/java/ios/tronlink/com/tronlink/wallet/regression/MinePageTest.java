3389aa27b14f5eaa90b4446f2387b9bce9b12245
hechuan <hccgk@sina.com>
2019-11-18 11:59:44 +0800

    add file

---
+++ src/test/java/ios/tronlink/com/tronlink/wallet/regression/MinePageTest.java
@@ -0,0 +1,13 @@
+package ios.tronlink.com.tronlink.wallet.regression;
+
+import ios.tronlink.com.tronlink.wallet.UITest.base.BaseTest;
+
+
+/**
+ * 我的页面功能测试
+ */
+public class MinePageTest extends BaseTest {
+
+
+
+}
