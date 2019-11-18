992a82d2c17de6d29571768b1d883b8c94996182
hechuan <hccgk@sina.com>
2019-11-18 12:49:58 +0800

    add files three

---
+++ src/test/java/ios/tronlink/com/tronlink/wallet/UITest/base/BaseTest.java
@@ -0,0 +1,28 @@
+package ios.tronlink.com.tronlink.wallet.UITest.base;
+
+import ios.tronlink.com.tronlink.wallet.utils.Helper;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+public class BaseTest  extends Base {
+
+    @Parameters({"privateKey"})
+    @BeforeClass(alwaysRun = true)
+    public void setUpBefore(String privateKey) throws Exception {
+        new Helper().getSign(privateKey,DRIVER);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void afterMethod(){
+        DRIVER.closeApp();
+        DRIVER.launchApp();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownAfterClass() {
+        DRIVER.quit();
+    }
+
+}
