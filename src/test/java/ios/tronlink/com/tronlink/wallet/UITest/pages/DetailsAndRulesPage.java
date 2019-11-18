e43b4d67a6953fc52ce45d38c342e3d86a6a99bb
hechuan <hccgk@sina.com>
2019-11-18 15:59:57 +0800

    Revert "Revert "Dev auto ioss make  organization""
    This reverts commit 231d3f3630761ca8329db9e5aa8a084e3d7e3042.

---
+++ src/test/java/ios/tronlink/com/tronlink/wallet/UITest/pages/DetailsAndRulesPage.java
@@ -0,0 +1,22 @@
+package ios.tronlink.com.tronlink.wallet.UITest.pages;
+
+import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class DetailsAndRulesPage extends AbstractPage {
+
+    public IOSDriver<?> driver;
+
+    public DetailsAndRulesPage(IOSDriver<?> driver) {
+        super(driver);
+        this.driver = driver;
+    }
+
+
+    @FindBy(id = "com.tronlink.wallet:id/tv_common_title")
+    public WebElement detailsAndRules_title;
+
+
+
+}
