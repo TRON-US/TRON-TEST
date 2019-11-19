d04e19980e7b465bc4c674cb43853fafc22403ab
hechuan <hccgk@sina.com>
2019-11-19 19:19:10 +0800

    revert AddAssetsTest

--- src/test/java/ios/tronlink/com/tronlink/wallet/regression/AddAssetsTest.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/regression/AddAssetsTest.java
@@ -16,29 +16,67 @@ public class AddAssetsTest extends BaseTest {
 
 
 
+//    @Test(description = "test add assert",alwaysRun = true)
+//    public void test002_addAsset() throws Exception {
+//        AssetPage asset = new AssetPage(DRIVER);
+//        AddAssertPage addAssert =  asset.enterAddAssertPage();
+//        SearchAssertPage searchAssert = addAssert.enterSearchAssertPage();
+//        searchAssert.addAssert_input.sendKeys("1000029");
+//        searchAssert.openAssert();
+//        TimeUnit.SECONDS.sleep(5);
+//        addAssert = searchAssert.enterAddAssertPage();
+//        addAssert.mainPageAssetManage_tab.click();
+//        Assert.assertTrue(addAssert.myNewAddAsset_btn.isDisplayed());
+//    }
+//
+//
+//    @Test(description = "test remove asset",alwaysRun = true)
+//    public void test003_removeAsset(){
+//        AssetPage asset = new AssetPage(DRIVER);
+//        AddAssertPage addAssert =  asset.enterAddAssertPage();
+//        addAssert.mainPageAssetManage_tab.click();
+//        addAssert.removeAsset();
+//        Assert.assertFalse(addAssert.switchFirst_btn.isSelected());
+//    }
+
     @Test(description = "test add assert",alwaysRun = true)
     public void test002_addAsset() throws Exception {
-        AssetPage asset = new AssetPage(DRIVER);
-        AddAssertPage addAssert =  asset.enterAddAssertPage();
-        SearchAssertPage searchAssert = addAssert.enterSearchAssertPage();
-        searchAssert.addAssert_input.sendKeys("1000029");
-        searchAssert.openAssert();
-        TimeUnit.SECONDS.sleep(5);
-        addAssert = searchAssert.enterAddAssertPage();
-        addAssert.mainPageAssetManage_tab.get(1).click();
-        Assert.assertTrue(addAssert.myNewAddAsset_btn.isDisplayed());
+        AssetPage assetPage = new AssetPage(DRIVER);
+        AddAssertPage addAssertPage = assetPage.enterAddAssertPage();
+        SearchAssertPage searchAssertPage =addAssertPage.enterSearchAssertPage();
+        searchAssertPage.addAssert_input.sendKeys("1000027");
+        searchAssertPage.keyboard_btn.click();
+        searchAssertPage.turnAsset_btn.click();
+        addAssertPage = searchAssertPage.enterAddAssertPage();
+        assetPage = addAssertPage.enterAssetPage();
+
+        //TimeUnit.SECONDS.sleep(2);
+        Assert.assertTrue(assetPage.myNewAddAsset_text.getText().contains("testAssetIssue_1567077083240"));
     }
+
 
 
     @Test(description = "test remove asset",alwaysRun = true)
-    public void test003_removeAsset(){
-        AssetPage asset = new AssetPage(DRIVER);
-        AddAssertPage addAssert =  asset.enterAddAssertPage();
-        addAssert.mainPageAssetManage_tab.get(1).click();
-        addAssert.removeAsset();
-        Assert.assertFalse(addAssert.switchFirst_btn.isSelected());
+    public void test003_removeAsset() throws Exception {
+        AssetPage assetPage = new AssetPage(DRIVER);
+        AddAssertPage addAssertPage =  assetPage.enterAddAssertPage();
+        addAssertPage.mainPageAssetManage_tab.click();
+        addAssertPage.hotAssetManage_tab.click();
+        addAssertPage.mainPageAssetManage_tab.click();
+        addAssertPage.hotAssetManage_tab.click();
+        addAssertPage.mainPageAssetManage_tab.click();
+        addAssertPage.hotAssetManage_tab.click();
+        addAssertPage.mainPageAssetManage_tab.click();
+        //TimeUnit.SECONDS.sleep(10);
+        //String pageSource = DRIVER.getPageSource();
+        //System.out.println(pageSource);
+        System.out.println("12121212312321---"+addAssertPage.assertSwitch_btn.size());
+        Assert.assertTrue(addAssertPage.assertSwitch_btn.size() >= 3);
+
+
+
+        //addAssertPage.removeAsset();
+        //Assert.assertFalse(addAssert.switchFirst_btn.isSelected());
     }
-
-
 
 }
