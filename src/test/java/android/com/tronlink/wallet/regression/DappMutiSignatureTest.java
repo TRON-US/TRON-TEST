3775772a7f054f58b9d6d305a0f64331015884a1
562389830@qq.com <gx19901002>
2019-11-19 17:06:12 +0800

    repair case

--- src/test/java/android/com/tronlink/wallet/regression/DappMutiSignatureTest.java
+++ src/test/java/android/com/tronlink/wallet/regression/DappMutiSignatureTest.java
@@ -119,13 +119,25 @@ public class DappMutiSignatureTest extends Base {
         ModifyPermissionPage modifyPermission = multiSignManager.enterModifyPermissionPage();
         multiSignManager = modifyPermission.modify(address);
         String signName = multiSignManager.permissionName_text.getText();
-        Assert.assertEquals(signName,"active_new");
+        System.out.println("get modify mulSign name is : " + signName);
+        Assert.assertEquals(signName,"active_est");
+    }
+
+
+    @Test(description = "signature is exist",alwaysRun = true)
+    public void test004_signatureIsExist() throws Exception {
+        MultiSignManagerPage multiSignManager =enterMultiSignManagerPage();
+        AddPermissionPage add = multiSignManager.enterAddPermissionPage();
+        add.permissionName_input.sendKeys("active_newest");
+        TimeUnit.SECONDS.sleep(1);
+        String tip = add.tip_hits.getText();
+        Assert.assertTrue(tip.contains("请更换名称") || tip.contains("one permission with this"));
     }
 
 
 
     @Test(description = "delete signature Test",alwaysRun = true)
-    public void test004_delSignature() throws Exception {
+    public void test005_delSignature() throws Exception {
         MultiSignManagerPage multiSignManager =enterMultiSignManagerPage();
         String signName = multiSignManager.permissionName_text.getText();
         multiSignManager.delSign();
@@ -134,16 +146,131 @@ public class DappMutiSignatureTest extends Base {
 
 
 
-    //Modify signature,Return to the before state
-    @Parameters({"address"})
-    @Test(description = "Modify signature Test",alwaysRun = true)
-    public void test005_modifySignature(String address) throws Exception {
-        MultiSignManagerPage multiSignManager =enterMultiSignManagerPage();
-        ModifyPermissionPage modifyPermission = multiSignManager.enterModifyPermissionPage();
-        multiSignManager = modifyPermission.modify(address);
-        String signName = multiSignManager.permissionName_text.getText();
-        Assert.assertEquals(signName,"active_newest");
+
+    @Test(description = "signature Name Is Null",alwaysRun = true)
+    public void test007_signatureNameIsNull() throws Exception {
+        MultiSignManagerPage multiSignManager = enterMultiSignManagerPage();
+        AddPermissionPage add = multiSignManager.enterAddPermissionPage();
+        Helper.swipScreen(DRIVER);
+        add.confirm_btn.click();
+        TimeUnit.SECONDS.sleep(1);
+        Helper.swipScreenToTop(DRIVER);
+        String tip = add.tip_hits.getText();
+        Assert.assertTrue(tip.contains("名字长度须") || tip.contains("Name length must be"));
     }
+
+
+
+    @Test(description = "signature Name Is too long",alwaysRun = true)
+    public void test008_signatureNameIsSoLong() throws Exception {
+        MultiSignManagerPage multiSignManager = enterMultiSignManagerPage();
+        AddPermissionPage add = multiSignManager.enterAddPermissionPage();
+        add.permissionName_input.sendKeys("TXtrbmfwZ2LxtoCveEhZT86fTss1w8rwJE");
+        TimeUnit.SECONDS.sleep(1);
+        String tip = add.tip_hits.getText();
+        Assert.assertTrue(tip.contains("名字长度须") || tip.contains("Name length must be"));
+    }
+
+
+
+    @Test(description = "signature without choise Permission",alwaysRun = true)
+    public void test009_signatureWithoutPermission() throws Exception {
+        MultiSignManagerPage multiSignManager = enterMultiSignManagerPage();
+        AddPermissionPage add = multiSignManager.enterAddPermissionPage();
+        add.inputInfoWithoutPermission("AutoTest");
+        String tip = add.operations_tip.getText();
+        Assert.assertTrue(tip.contains("选择一个") || tip.contains("Please select"));
+    }
+
+
+    @Test(description = "signature threshold > 100",alwaysRun = true)
+    public void test010_thresholdTooLarge() throws Exception {
+        MultiSignManagerPage multiSignManager = enterMultiSignManagerPage();
+        AddPermissionPage add = multiSignManager.enterAddPermissionPage();
+        add.threshold_input.sendKeys("101");
+        //add.inputInfoWithoutPermission("AutoTest");
+        TimeUnit.SECONDS.sleep(1);
+        String tip = add.threshold_tip.getText();
+        Assert.assertTrue(tip.contains("阈值须≤100") || tip.contains("threhold value of"));
+    }
+
+
+
+    @Test(description = "signature threshold Is 0",alwaysRun = true)
+    public void test011_thresholdIsZero() throws Exception {
+        MultiSignManagerPage multiSignManager = enterMultiSignManagerPage();
+        AddPermissionPage add = multiSignManager.enterAddPermissionPage();
+        add.threshold_input.sendKeys("0");
+        //add.inputInfoWithoutPermission("AutoTest");
+        TimeUnit.SECONDS.sleep(1);
+        String tip = add.threshold_tip.getText();
+        Assert.assertTrue(tip.contains("阈值须≤100") || tip.contains("threhold value of"));
+    }
+
+
+
+
+    @Test(description = "signature with error Adress",alwaysRun = true)
+    public void test012_errorAdress() throws Exception {
+        MultiSignManagerPage multiSignManager = enterMultiSignManagerPage();
+        AddPermissionPage add = multiSignManager.enterAddPermissionPage();
+        add.address_input.get(0).sendKeys("AAtrbmfwZ2LxtoCveEhZT86fTss1w8rwJE");
+        TimeUnit.SECONDS.sleep(1);
+        String tip = add.addkey_tip.getText();
+        Assert.assertTrue(tip.equals("请填写正确的地址") || tip.contains("enter the correct address"));
+    }
+
+
+
+    @Test(description = "Adress Is Null",alwaysRun = true)
+    public void test013_AdressIsNull() throws Exception {
+        MultiSignManagerPage multiSignManager = enterMultiSignManagerPage();
+        AddPermissionPage add = multiSignManager.enterAddPermissionPage();
+        Helper.swipScreen(DRIVER);
+        add.confirm_btn.click();
+        TimeUnit.SECONDS.sleep(1);
+        String tip = add.addkey_tip.getText();
+        Assert.assertTrue(tip.equals("请填写正确的地址") || tip.contains("enter the correct address"));
+    }
+
+
+
+    @Test(description = "two Adress is equals",alwaysRun = true)
+    public void test014_adressIsEquals() throws Exception {
+        MultiSignManagerPage multiSignManager = enterMultiSignManagerPage();
+        AddPermissionPage add = multiSignManager.enterAddPermissionPage();
+        //add.address_input.sendKeys("TKG4UtDejJfAQx3FsyAUs86cpcRzYcijth");
+        add.inputSameAddress();
+        TimeUnit.SECONDS.sleep(1);
+        String tip = add.addkey_tip.getText();
+        Assert.assertTrue(tip.contains("key重复") || tip.contains("has been added"));
+    }
+
+
+
+    @Test(description = "password is null",alwaysRun = true)
+    public void test015_passwordIsNull() throws Exception {
+        MultiSignManagerPage multiSignManager = enterMultiSignManagerPage();
+        AddPermissionPage add = multiSignManager.enterAddPermissionPage();
+        add.inputInfo("AutoTest");
+        add.send_btn.click();
+        TimeUnit.SECONDS.sleep(3);
+        Assert.assertTrue(add.inputPassword_title.isDisplayed());
+    }
+
+
+
+    @Test(description = "password is wrong",alwaysRun = true)
+    public void test016_passwordIsWrong() throws Exception {
+        MultiSignManagerPage multiSignManager = enterMultiSignManagerPage();
+        AddPermissionPage add = multiSignManager.enterAddPermissionPage();
+        add.inputInfo("AutoTest");
+        add.password_input.sendKeys("error_password");
+        add.send_btn.click();
+        TimeUnit.SECONDS.sleep(3);
+        Assert.assertTrue(add.inputPassword_title.isDisplayed());
+    }
+
 
 
 
