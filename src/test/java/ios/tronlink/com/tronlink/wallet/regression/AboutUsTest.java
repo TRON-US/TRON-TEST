2a4fcf0e4f257a6da80b5bf0dc299c59506dc477
hechuan <hccgk@sina.com>
2019-11-19 20:39:20 +0800

    add Files and and 1 case in AboutUsTest

--- src/test/java/ios/tronlink/com/tronlink/wallet/regression/AboutUsTest.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/regression/AboutUsTest.java
@@ -2,6 +2,12 @@ package ios.tronlink.com.tronlink.wallet.regression;
 
 
 import ios.tronlink.com.tronlink.wallet.UITest.base.BaseTest;
+import ios.tronlink.com.tronlink.wallet.UITest.pages.AboutUsPage;
+import ios.tronlink.com.tronlink.wallet.UITest.pages.AssetPage;
+import ios.tronlink.com.tronlink.wallet.UITest.pages.MinePage;
+import ios.tronlink.com.tronlink.wallet.UITest.pages.VersionsPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 
 /**
@@ -9,17 +15,14 @@ import ios.tronlink.com.tronlink.wallet.UITest.base.BaseTest;
  */
 public class AboutUsTest extends BaseTest {
 
-//    @BeforeClass
-//    public void setUpBeforeClass() throws Exception {
-//        setUp();
-//    }
+    @Test(description = "test AboutUs assert",alwaysRun = true)
+    public void test001_aboutUs() throws Exception {
+        AssetPage assetPage = new AssetPage(DRIVER);
+        MinePage minePage =  assetPage.enterMinePage();
+        AboutUsPage aboutusPage = minePage.enterAboutUsPage();
+        VersionsPage versionsPage = aboutusPage.enterVersionsPage();
+        Assert.assertEquals(versionsPage.title.getText(),"版本日志");
+    }
 
-//    @Parameters({"privateKey"})
-//    @BeforeMethod()
-//    public void setUpBefore(String privateKey) throws Exception{
-//        DRIVER.closeApp();
-//        DRIVER.launchApp();
-//        getSign(privateKey);
-//    }
 
 }
