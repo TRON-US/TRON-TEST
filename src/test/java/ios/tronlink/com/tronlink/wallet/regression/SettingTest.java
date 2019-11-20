c8e64669aa2c411dd17931dbda6331d203870390
hechuan <hccgk@sina.com>
2019-11-20 14:44:58 +0800

    add SettingTest case

--- src/test/java/ios/tronlink/com/tronlink/wallet/regression/SettingTest.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/regression/SettingTest.java
@@ -2,10 +2,7 @@ package ios.tronlink.com.tronlink.wallet.regression;
 
 
 import ios.tronlink.com.tronlink.wallet.UITest.base.BaseTest;
-import ios.tronlink.com.tronlink.wallet.UITest.pages.AssetPage;
-import ios.tronlink.com.tronlink.wallet.UITest.pages.DAPP_BrowerPage;
-import ios.tronlink.com.tronlink.wallet.UITest.pages.MinePage;
-import ios.tronlink.com.tronlink.wallet.UITest.pages.SettingPage;
+import ios.tronlink.com.tronlink.wallet.UITest.pages.*;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -16,32 +13,102 @@ public class SettingTest extends BaseTest {
 
 
 
-    @Test(description = "switch Language Test")
-    public void test002_bulletin() throws Exception {
+    @Test(description = "into Chain Set Page Test")
+    public void test001_bulletin() throws Exception {
         AssetPage asset = new AssetPage(DRIVER);
         MinePage mine = asset.enterMinePage();
         SettingPage setting = mine.enterSettingPage();
+        NodeSetPage nodeSetPage = setting.enterNodeSetPage();
+        Assert.assertEquals(nodeSetPage.title.getText(),"设置");
+
     }
 
-    @Test(description = "Developer options Test",alwaysRun = true)
-    public void test002_developerOptions() throws Exception {
+    @Test(description = "Developer options On Test",alwaysRun = true)
+    public void test002_developerOnOptions() throws Exception {
         AssetPage asset = new AssetPage(DRIVER);
         MinePage mine = asset.enterMinePage();
         SettingPage setting = mine.enterSettingPage();
-        String developer = setting.testnode_text.getText();
         setting.trunDeveloperOptions();
-        String developerNow = setting.testnode_text.getText();
-        Assert.assertNotEquals(developer,developerNow);
+        String developerNow = setting.connected_title.getText();
+        Assert.assertEquals(developerNow,"已选择shasta网络");
     }
-
-    @Test(description = "DAPP Browser Test",alwaysRun = true)
-    public void test003_DAPP_Browser() throws Exception {
+    @Test(description = "Developer options Off Test",alwaysRun = true)
+    public void test003_developerOffOptions() throws Exception {
         AssetPage asset = new AssetPage(DRIVER);
         MinePage mine = asset.enterMinePage();
         SettingPage setting = mine.enterSettingPage();
-        DAPP_BrowerPage dapp =  setting.enterDAPP_BrowerPage();
-        dapp.testUrl();
-        Assert.assertEquals("TEST",dapp.dappTtile_btn.getText());
+        setting.trunOffDeveloperOptions();
+        String developerNow = setting.disconnected_title.getText();
+        Assert.assertEquals(developerNow,"未选择");
+    }
+
+    @Test(description = "Language Switch Test",alwaysRun = true)
+    public void test004_Languagetest() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        MinePage mine = asset.enterMinePage();
+        SettingPage setting = mine.enterSettingPage();
+        setting.languane_btn.click();
+        setting.Cancal_btn.click();
+        setting.languane_btn.click();
+        setting.english_btn.click();
+        setting.languaneEn_btn.click();
+        setting.languaneCn_btn.click();
+    }
+
+    @Test(description = "currencyUnit Switch Test",alwaysRun = true)
+    public void test005_currencyUnittest() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        MinePage mine = asset.enterMinePage();
+        SettingPage setting = mine.enterSettingPage();
+        setting.currencyUnit_btn.click();
+        setting.USDUnit_btn.click();
+        setting.USDUnit_btn.click();
+        setting.CNYUnit_btn.click();
+        setting.CNYUnit_btn.click();
+        setting.Cancal_btn.click();
+
+    }
+
+    @Test(description = "SideChain Test",alwaysRun = true)
+    public void test006_SideChaintest() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        MinePage mine = asset.enterMinePage();
+        SettingPage setting = mine.enterSettingPage();
+        NodeSetPage nodeSetPage = setting.enterNodeSetPage();
+        NodeSetDetailPage detailPage =  nodeSetPage.enterSettingPageMainChain();
+        NodeSetPage nodeSetPage1 = detailPage.backToAction();
+//        NodeSetDetailPage detailPaged =  nodeSetPage1.enterSettingPageDAppChain();
+//        NodeSetPage nodeSetPage2 = detailPaged.backToAction();
+        Assert.assertNotNull(nodeSetPage1.title);
+    }
+
+    @Test(description = "Into Mnemonic Page Test",alwaysRun = true)
+    public void test007_IntoMnemonicPagetest() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        MinePage mine = asset.enterMinePage();
+        SettingPage setting = mine.enterSettingPage();
+        MnemonicToolsPage toolsPage = setting.enternemTools_btnPage();
+        Assert.assertEquals(toolsPage.title.getText(),"助记词转换工具");
+    }
+    @Test(description = "Mnemonic Exchange Test",alwaysRun = true)
+    public void test008_MnemonicExchangetest() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        MinePage mine = asset.enterMinePage();
+        SettingPage setting = mine.enterSettingPage();
+        MnemonicToolsPage toolsPage = setting.enternemTools_btnPage();
+        toolsPage.switchMnems();
+        Assert.assertNotNull(toolsPage.assetExpectText);
+
+    }
+
+    @Test(description = "DAPP BrowserPage Test",alwaysRun = true)
+    public void test009_DAPP_Browser()  {
+        AssetPage asset = new AssetPage(DRIVER);
+        MinePage mine = asset.enterMinePage();
+        SettingPage setting = mine.enterSettingPage();
+        DAPP_BrowerPage dapp1 =  setting.enterDAPP_BrowerPage();
+        dapp1.testUrl();
+//        Assert.assertEquals("TEST",dapp.dappTtile_btn.getText());
     }
 
 
