6792774e458627754b7a5926b00ee6de4ca10fbd
hechuan <hccgk@sina.com>
2019-11-23 16:41:43 +0800

    conflict

--- src/test/java/ios/tronlink/com/tronlink/wallet/UITest/pages/SendTrxPage.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/UITest/pages/SendTrxPage.java
@@ -100,10 +100,7 @@ public class SendTrxPage extends AbstractPage {
         WebElement element = driver.findElementByIosNsPredicate("type == 'XCUIElementTypeButton' AND name == '发送'");
         return element;
     }
-<<<<<<< HEAD
-=======
 
->>>>>>> add_case_ipa
     public void broadcastButtonClick(){
 
         WebElement element = driver.findElementByIosNsPredicate("type == 'XCUIElementTypeButton' AND name == '完成'");
