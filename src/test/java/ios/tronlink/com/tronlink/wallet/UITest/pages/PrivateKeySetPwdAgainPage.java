32c58b2fa43d99a7a2e983f98908d8423b14e164
hechuan <hccgk@sina.com>
2019-11-26 17:24:37 +0800

    add case

--- src/test/java/ios/tronlink/com/tronlink/wallet/UITest/pages/PrivateKeySetPwdAgainPage.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/UITest/pages/PrivateKeySetPwdAgainPage.java
@@ -1,9 +1,11 @@
 package ios.tronlink.com.tronlink.wallet.UITest.pages;
 
 import io.appium.java_client.ios.IOSDriver;
+import ios.tronlink.com.tronlink.wallet.utils.Helper;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class PrivateKeySetPwdAgainPage extends AbstractPage {
@@ -14,24 +16,27 @@ public class PrivateKeySetPwdAgainPage extends AbstractPage {
         this.driver = driver;
     }
 
-    @FindBy(name = "com.tronlink.wallet:id/et_password")
+    @FindBy(className = "XCUIElementTypeSecureTextField")
     public WebElement pwd_input;
 
-
+    @FindBy(className = "XCUIElementTypeButton" )
+    public List<WebElement> error_hits;
 
     @FindBy(name = "com.tronlink.wallet:id/creat")
     public WebElement create_btn;
 
+    public String  getError_hits(){
+        return error_hits.get(2).getText();
+    }
 
-
-    @FindBy(name = "com.tronlink.wallet:id/tv_error")
-    public WebElement error_hits;
-
-
+    public WebElement getComplish_btn(){
+        return driver.findElementByIosNsPredicate("type = 'XCUIElementTypeButton' AND name = '完成'");
+    }
 
     public AssetPage enterAssetPage(String pwdAgain) throws Exception {
         pwd_input.sendKeys(pwdAgain);
-        create_btn.click();
+        Helper.tapWhitePlace(driver);
+        getComplish_btn().click();
         TimeUnit.SECONDS.sleep(1);
         return new AssetPage(driver);
     }
