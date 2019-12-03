78ea1c1eec7366a8139b7a33af7d7d2233f225f4
562389830@qq.com <gx19901002>
2019-12-03 18:15:01 +0800

    update newest apk ...

--- src/test/java/android/com/tronlink/wallet/regression/FrozenAndUnfreezeTest.java
+++ src/test/java/android/com/tronlink/wallet/regression/FrozenAndUnfreezeTest.java
@@ -62,7 +62,7 @@ public class FrozenAndUnfreezeTest extends Base {
         AssetPage asset = new AssetPage(DRIVER);
         FrozenAndUnfreezePage frozen = asset.enterFrozenAndThawingPage();
         frozen.freezeEnergyDetail_btn.click();
-        int myFreeze = Integer.valueOf(frozen.myFreeze_btn.getText());
+        int myFreeze = Integer.valueOf(removeSymbol(frozen.myFreeze_btn.getText()));
         int otherFreeze = Integer.valueOf(removeSymbol(frozen.otherFreeze_btn.getText()));
         int totalFreeze = Integer.valueOf(removeSymbol(frozen.totalFreeze_btn.getText()));
         Assert.assertTrue(myFreeze + otherFreeze == totalFreeze);
