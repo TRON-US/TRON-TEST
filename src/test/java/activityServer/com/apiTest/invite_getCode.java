670a018193e40a904d3057cb9465907790a6a08c
kaizhu@tron.network <kaizhu@tron.network>
2019-12-10 17:34:53 +0800

    add test case

--- src/test/java/activityServer/com/apiTest/invite_getCode.java
+++ src/test/java/activityServer/com/apiTest/invite_getCode.java
@@ -1,6 +1,23 @@
 package activityServer.com.apiTest;
 
-public class invite_getCode {
+import activityServer.com.utils.api;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpResponse;
+import org.testng.annotations.Test;
 
+public class invite_getCode {
+  private HttpResponse response;
+  private JSONObject responseContent;
+  private JSONObject addressJson = new JSONObject();
+
+  @Test(enabled = true, description = "Api POST /api/wallet/invite/get_code test")
+  public void test001AllClassAsset() throws Exception {
+    addressJson.put("address", api.testAddressBase64);
+    response = api.getInviteCode(addressJson);
+
+    JSONObject inviteCodeinfo = api.parseResponseContent(response);
+    api.printJsonContent(inviteCodeinfo);
+  }
 
 }
