2d5b6535a7d416869dab59b3805700906c0763f3
kaizhu@tron.network <kaizhu@tron.network>
2019-12-11 16:35:49 +0800

    upgrade

--- src/test/java/activityServer/com/utils/api.java
+++ src/test/java/activityServer/com/utils/api.java
@@ -64,10 +64,16 @@ public class api {
         httpClient = new DefaultHttpClient(pccm);
     }
 
-    public static HttpResponse upgrade() throws Exception{
+    public static HttpResponse upgrade(HashMap<String,String> header) throws Exception{
         final String requestUrl = HttpNode + "/api/v1/wallet/upgrade";
-        System.out.println(requestUrl);
-        response = createGetConnect(requestUrl);
+        URIBuilder builder = new URIBuilder(requestUrl);
+        if (header != null) {
+            for (String key : header.keySet()) {
+                builder.addParameter(key, header.get(key));
+            }
+        }
+        URI uri = builder.build();
+        response = createGetConnect(uri,header);
         Assert.assertTrue(api.verificationResult(response));
         return response;
     }
@@ -193,6 +199,20 @@ public class api {
         Assert.assertTrue(api.verificationResult(response));
         return response;
     }
+    public static HttpResponse lotteryRecord(HashMap<String,String> param) throws Exception{
+        final String requesturl = HttpNode + "/api/wallet/lottery/record";
+        URIBuilder builder = new URIBuilder(requesturl);
+        if (param != null) {
+            for (String key : param.keySet()) {
+                builder.addParameter(key, param.get(key));
+            }
+        }
+        URI uri = builder.build();
+        System.out.println(uri);
+        response = createGetConnect(uri);
+        Assert.assertTrue(api.verificationResult(response));
+        return response;
+    }
     public static HttpResponse inviteLeaderBoard(HashMap<String,String> param) throws Exception{
         final String requesturl = HttpNode + "/api/wallet/invite/list";
         URIBuilder builder = new URIBuilder(requesturl);
@@ -303,6 +323,26 @@ public class api {
             httpGet = new HttpGet(uri);
             httpGet.setHeader("Content-type", "application/json; charset=utf-8");
             httpGet.setHeader("Connection", "Close");
+            response = httpClient.execute(httpGet);
+        } catch (Exception e) {
+            e.printStackTrace();
+            httpGet.releaseConnection();
+            return null;
+        }
+        return response;
+    }
+
+    public static HttpResponse createGetConnect(URI uri,HashMap<String,String> header) {
+        try {
+            httpClient.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT,
+                    connectionTimeout);
+            httpClient.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, soTimeout);
+            httpGet = new HttpGet(uri);
+            httpGet.setHeader("Content-type", "application/json; charset=utf-8");
+            httpGet.setHeader("Connection", "Close");
+            for (HashMap.Entry<String,String> entry : header.entrySet()){
+                httpGet.setHeader(entry.getKey(),entry.getValue());
+            }
             response = httpClient.execute(httpGet);
         } catch (Exception e) {
             e.printStackTrace();
