0bbc8930088e5cec08010424002b50942543ca32
kaizhu@tron.network <kaizhu@tron.network>
2019-12-11 11:28:25 +0800

    add nodeInfo test case

--- src/test/java/activityServer/com/apiTest/invite_getCode.java
+++ src/test/java/activityServer/com/apiTest/invite_getCode.java
@@ -9,12 +9,12 @@ import org.testng.annotations.Test;
 public class invite_getCode {
   private HttpResponse response;
   private JSONObject responseContent;
-  private JSONObject addressJson = new JSONObject();
+  private JSONObject parameter = new JSONObject();
 
   @Test(enabled = true, description = "Api POST /api/wallet/invite/get_code test")
   public void test001AllClassAsset() throws Exception {
-    addressJson.put("address", api.testAddressBase64);
-    response = api.getInviteCode(addressJson);
+    parameter.put("address", api.testAddressBase64);
+    response = api.getInviteCode(parameter);
 
     JSONObject inviteCodeinfo = api.parseResponseContent(response);
     api.printJsonContent(inviteCodeinfo);
