63e70fd0395e011cf5156630b11306b334f655ea
hechuan <hccgk@sina.com>
2019-12-12 16:39:46 +0800

    add addassettoken test case In releaseNet

--- src/test/java/ios/tronlink/com/tronlink/wallet/regression/AddAssetsTest.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/regression/AddAssetsTest.java
@@ -5,10 +5,10 @@ import ios.tronlink.com.tronlink.wallet.UITest.pages.AddAssertPage;
 import ios.tronlink.com.tronlink.wallet.UITest.pages.AssetPage;
 import ios.tronlink.com.tronlink.wallet.UITest.pages.SearchAssertPage;
 import ios.tronlink.com.tronlink.wallet.utils.Helper;
-import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
+
 
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
@@ -16,6 +16,53 @@ import static org.testng.Assert.assertTrue;
 
 public class AddAssetsTest extends BaseTest {
 
+
+
+    @Test(description = "test add assert",alwaysRun = true)
+    public void test002_addAsset() throws Exception {
+        Helper.switchToReleaseNet(DRIVER);
+        DRIVER.closeApp();
+        DRIVER.launchApp();
+
+        AssetPage assetPage = new AssetPage(DRIVER);
+        AddAssertPage addAssertPage = assetPage.enterAddAssertPage();
+        SearchAssertPage searchAssertPage =addAssertPage.enterSearchAssertPage();
+        searchAssertPage.addAssert_input.sendKeys("888Tron");
+        searchAssertPage.openAssert();
+        addAssertPage = searchAssertPage.enterAddAssertPage();
+        assetPage = addAssertPage.enterAssetPage();
+        Helper.swipScreen(DRIVER);
+        TimeUnit.SECONDS.sleep(2);
+        assertTrue(Helper.isElementExist(assetPage.driver,"888"));
+    }
+
+
+
+    @Test(description = "test remove asset",alwaysRun = true)
+    public void test003_removeAsset() throws Exception {
+        Helper.switchToReleaseNet(DRIVER);
+        DRIVER.closeApp();
+        DRIVER.launchApp();
+
+        AssetPage assetPage = new AssetPage(DRIVER);
+        AddAssertPage addAssertPage =  assetPage.enterAddAssertPage();
+        addAssertPage.mainPageAssetManage_tab.click();
+        addAssertPage.hotAssetManage_tab.click();
+        addAssertPage.mainPageAssetManage_tab.click();
+        addAssertPage.hotAssetManage_tab.click();
+        addAssertPage.mainPageAssetManage_tab.click();
+        addAssertPage.hotAssetManage_tab.click();
+        addAssertPage.mainPageAssetManage_tab.click();
+        Helper.swipScreen(DRIVER);
+        TimeUnit.SECONDS.sleep(2);
+        addAssertPage.cloeAssert();
+        assetPage =  addAssertPage.enterAssetPage();
+        Helper.swipScreen(DRIVER);
+        TimeUnit.SECONDS.sleep(2);
+        assertFalse(Helper.isElementExist(assetPage.driver,"888"));
+        Helper.switchToDevNet(DRIVER);
+
+    }
 
 
 
