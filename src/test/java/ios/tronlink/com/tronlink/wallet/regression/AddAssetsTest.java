305f52f75919691c6d9ddf2247b981c7dbcae376
hechuan <hccgk@sina.com>
2019-12-13 16:33:45 +0800

    addasset

--- src/test/java/ios/tronlink/com/tronlink/wallet/regression/AddAssetsTest.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/regression/AddAssetsTest.java
@@ -17,73 +17,33 @@ import static org.testng.Assert.assertTrue;
 public class AddAssetsTest extends BaseTest {
 
 
-
-    @Test(description = "test add assert",alwaysRun = true)
-    public void test002_addAsset() throws Exception {
-        Helper.switchToReleaseNet(DRIVER);
-        DRIVER.closeApp();
-        DRIVER.launchApp();
-
-        AssetPage assetPage = new AssetPage(DRIVER);
-        AddAssertPage addAssertPage = assetPage.enterAddAssertPage();
-        SearchAssertPage searchAssertPage =addAssertPage.enterSearchAssertPage();
-        searchAssertPage.addAssert_input.sendKeys("888Tron");
-        searchAssertPage.openAssert();
-        addAssertPage = searchAssertPage.enterAddAssertPage();
-        assetPage = addAssertPage.enterAssetPage();
-        Helper.swipScreen(DRIVER);
-        TimeUnit.SECONDS.sleep(2);
-        assertTrue(Helper.isElementExist(assetPage.driver,"888"));
-    }
-
-
-
-    @Test(description = "test remove asset",alwaysRun = true)
-    public void test003_removeAsset() throws Exception {
-        Helper.switchToReleaseNet(DRIVER);
-        DRIVER.closeApp();
-        DRIVER.launchApp();
-
-        AssetPage assetPage = new AssetPage(DRIVER);
-        AddAssertPage addAssertPage =  assetPage.enterAddAssertPage();
-        addAssertPage.mainPageAssetManage_tab.click();
-        addAssertPage.hotAssetManage_tab.click();
-        addAssertPage.mainPageAssetManage_tab.click();
-        addAssertPage.hotAssetManage_tab.click();
-        addAssertPage.mainPageAssetManage_tab.click();
-        addAssertPage.hotAssetManage_tab.click();
-        addAssertPage.mainPageAssetManage_tab.click();
-        Helper.swipScreen(DRIVER);
-        TimeUnit.SECONDS.sleep(2);
-        addAssertPage.cloeAssert();
-        assetPage =  addAssertPage.enterAssetPage();
-        Helper.swipScreen(DRIVER);
-        TimeUnit.SECONDS.sleep(2);
-        assertFalse(Helper.isElementExist(assetPage.driver,"888"));
-        Helper.switchToDevNet(DRIVER);
-
-    }
-
-
-
+//
 //    @Test(description = "test add assert",alwaysRun = true)
 //    public void test002_addAsset() throws Exception {
+//        Helper.switchToReleaseNet(DRIVER);
+//        DRIVER.closeApp();
+//        DRIVER.launchApp();
+//
 //        AssetPage assetPage = new AssetPage(DRIVER);
 //        AddAssertPage addAssertPage = assetPage.enterAddAssertPage();
 //        SearchAssertPage searchAssertPage =addAssertPage.enterSearchAssertPage();
-//        searchAssertPage.addAssert_input.sendKeys("1000027");
-//        searchAssertPage.keyboard_btn.click();
-//        searchAssertPage.turnAsset_btn.click();
+//        searchAssertPage.addAssert_input.sendKeys("888Tron");
+//        searchAssertPage.openAssert();
 //        addAssertPage = searchAssertPage.enterAddAssertPage();
 //        assetPage = addAssertPage.enterAssetPage();
+//        Helper.swipScreen(DRIVER);
 //        TimeUnit.SECONDS.sleep(2);
-//        assertTrue(Helper.isElementExist(assetPage.driver,"testAssetIssue_1567077083240"));
+//        assertTrue(Helper.isElementExist(assetPage.driver,"888"));
 //    }
 //
 //
 //
 //    @Test(description = "test remove asset",alwaysRun = true)
 //    public void test003_removeAsset() throws Exception {
+//        Helper.switchToReleaseNet(DRIVER);
+//        DRIVER.closeApp();
+//        DRIVER.launchApp();
+//
 //        AssetPage assetPage = new AssetPage(DRIVER);
 //        AddAssertPage addAssertPage =  assetPage.enterAddAssertPage();
 //        addAssertPage.mainPageAssetManage_tab.click();
@@ -93,10 +53,19 @@ public class AddAssetsTest extends BaseTest {
 //        addAssertPage.mainPageAssetManage_tab.click();
 //        addAssertPage.hotAssetManage_tab.click();
 //        addAssertPage.mainPageAssetManage_tab.click();
-//        addAssertPage.turnAsset_btn.click();
-//        assetPage =  addAssertPage.enterAssetPage();
+//        Helper.swipScreen(DRIVER);
 //        TimeUnit.SECONDS.sleep(2);
-//        assertFalse(Helper.isElementExist(assetPage.driver,"testAssetIssue_1567077083240"));
+//        addAssertPage.cloeAssert();
+//        assetPage =  addAssertPage.enterAssetPage();
+//        Helper.swipScreen(DRIVER);
+//        TimeUnit.SECONDS.sleep(2);
+//        assertFalse(Helper.isElementExist(assetPage.driver,"888"));
+//        Helper.switchToDevNet(DRIVER);
+//
+//        DRIVER.closeApp();
+//        DRIVER.launchApp();
 //    }
+
+
 
 }
