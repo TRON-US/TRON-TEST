aefbee8b984a0c6f2d22f8b679655a4931568dfd
kaizhu@tron.network <kaizhu@tron.network>
2019-12-24 17:21:06 +0800

    change test case

--- src/test/java/activityServer/com/apiTest/multiTrxRecord.java
+++ src/test/java/activityServer/com/apiTest/multiTrxRecord.java
@@ -19,8 +19,38 @@ public class multiTrxRecord {
     private JSONObject responseContent;
     private HashMap<String,String> param = new HashMap<>();
 
-    @Test(enabled = true, description = "Api upgrade test")
-    public void multiTrxRecord() throws Exception {
+    @Test(enabled = true, description = "Api multiTrxReword test")
+    public void multiTrxRecord0() throws Exception {
+        param.put("address","TRqgwhHbfscXq3Ym3FJSFwxprpto1S4nSW");
+        param.put("start","0");
+        param.put("limit","10");
+        param.put("state","0");
+        param.put("netType","main_net");
+        response = api.multiTrxReword(param);
+        Assert.assertTrue(api.verificationResult(response));
+        responseContent = api.parseResponseContent(response);
+        api.printJsonContent(responseContent);
+        Assert.assertTrue(responseContent.size() >= 3);
+        Assert.assertTrue(responseContent.getString("message").equals("OK"));
+
+        JSONArray array = responseContent.getJSONObject("data").getJSONArray("data");
+        for (Object signatureProgress:array) {
+            JSONObject info = (JSONObject) signatureProgress;
+            Assert.assertEquals(info.getString("state"),"0");
+            Assert.assertTrue(!info.getJSONArray("signatureProgress").isEmpty());
+            Assert.assertTrue(!info.getString("originatorAddress").isEmpty());
+            Assert.assertTrue(!info.getString("currentWeight").isEmpty());
+            Assert.assertTrue(!info.getString("contractType").isEmpty());
+            Assert.assertTrue(!info.getString("expireTime").isEmpty());
+            Assert.assertTrue(!info.getString("threshold").isEmpty());
+            Assert.assertTrue(!info.getString("isSign").isEmpty());
+            Assert.assertTrue(!info.getString("state").isEmpty());
+            Assert.assertTrue(!info.getString("hash").isEmpty());
+        }
+    }
+
+    @Test(enabled = true, description = "Api multiTrxReword test")
+    public void multiTrxRecord1() throws Exception {
         param.put("address","TRqgwhHbfscXq3Ym3FJSFwxprpto1S4nSW");
         param.put("start","0");
         param.put("limit","10");
@@ -37,15 +67,47 @@ public class multiTrxRecord {
         for (Object signatureProgress:array) {
             JSONObject info = (JSONObject) signatureProgress;
             Assert.assertEquals(info.getString("state"),"1");
+            Assert.assertTrue(!info.getJSONArray("signatureProgress").isEmpty());
             Assert.assertTrue(!info.getString("originatorAddress").isEmpty());
             Assert.assertTrue(!info.getString("currentWeight").isEmpty());
             Assert.assertTrue(!info.getString("contractType").isEmpty());
+            Assert.assertTrue(!info.getString("expireTime").isEmpty());
             Assert.assertTrue(!info.getString("threshold").isEmpty());
             Assert.assertTrue(!info.getString("isSign").isEmpty());
             Assert.assertTrue(!info.getString("state").isEmpty());
             Assert.assertTrue(!info.getString("hash").isEmpty());
-
-
         }
     }
+
+    @Test(enabled = true, description = "Api multiTrxReword test")
+    public void multiTrxRecord2() throws Exception {
+        param.put("address","TRqgwhHbfscXq3Ym3FJSFwxprpto1S4nSW");
+        param.put("start","0");
+        param.put("limit","10");
+        param.put("state","2");
+        param.put("netType","main_net");
+        response = api.multiTrxReword(param);
+        Assert.assertTrue(api.verificationResult(response));
+        responseContent = api.parseResponseContent(response);
+        api.printJsonContent(responseContent);
+        Assert.assertTrue(responseContent.size() >= 3);
+        Assert.assertTrue(responseContent.getString("message").equals("OK"));
+
+        JSONArray array = responseContent.getJSONObject("data").getJSONArray("data");
+        for (Object signatureProgress:array) {
+            JSONObject info = (JSONObject) signatureProgress;
+            Assert.assertEquals(info.getString("state"),"2");
+            Assert.assertTrue(!info.getJSONArray("signatureProgress").isEmpty());
+            Assert.assertTrue(!info.getString("originatorAddress").isEmpty());
+            Assert.assertTrue(!info.getString("currentWeight").isEmpty());
+            Assert.assertTrue(!info.getString("contractType").isEmpty());
+            Assert.assertTrue(!info.getString("expireTime").isEmpty());
+            Assert.assertTrue(!info.getString("threshold").isEmpty());
+            Assert.assertTrue(!info.getString("isSign").isEmpty());
+            Assert.assertTrue(!info.getString("state").isEmpty());
+            Assert.assertTrue(!info.getString("hash").isEmpty());
+        }
+    }
+
+
 }
