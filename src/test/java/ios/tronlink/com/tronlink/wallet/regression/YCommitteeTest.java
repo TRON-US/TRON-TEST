63c9836f0132b95e6057b7d1eee7e099b88649f8
hechuan <hccgk@sina.com>
2019-12-31 17:30:41 +0800

    time reduce 1min

--- src/test/java/ios/tronlink/com/tronlink/wallet/regression/YCommitteeTest.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/regression/YCommitteeTest.java
@@ -92,7 +92,7 @@ public class YCommitteeTest extends BaseTest {
             min.applyPattern("mm");// a为am/pm的标记
             Date date = new Date();// 获取当前时间
             System.out.println( "分钟数:" + min.format(date));
-            if(Integer.parseInt( min.format(date))%10 == 0)
+            if(Integer.parseInt( min.format(date))%10 == 9)
             {
                 break;
             }
@@ -100,12 +100,9 @@ public class YCommitteeTest extends BaseTest {
             Helper.swipRefreshScreen(DRIVER);
         }
 
-        SimpleDateFormat formart = new SimpleDateFormat();// 格式化时间
-        formart.applyPattern("HH:mm:ss");
-        Date date = new Date();
-        System.out.println(" 开始执行于: " + formart.format(date));
-
         committeePage.Setuppropos.click();
+
+        log("开始执行时间");
         String count = String.format("%.0f", Math.random() * 100000);
         myChangeCount = count;
         System.out.println(count);
