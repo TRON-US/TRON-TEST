b8fca31b050d2c5f27b7b89991bbd4c1b5261aad
hechuan <hccgk@sina.com>
2020-01-20 18:35:40 +0800

    1.add Transfer record Test 2.reconstruction Transfer order and name 、file name

--- src/test/java/ios/tronlink/com/tronlink/wallet/regression/DappSendTrxTest.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/regression/DappSendTrxTest.java
@@ -1,9 +1,5 @@
 package ios.tronlink.com.tronlink.wallet.regression;
 
-import io.appium.java_client.TouchAction;
-import ios.tronlink.com.tronlink.wallet.UITest.base.BaseTest;
-import ios.tronlink.com.tronlink.wallet.UITest.pages.*;
-import ios.tronlink.com.tronlink.wallet.utils.Helper;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
@@ -11,8 +7,17 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class DappChainSendTrxTest extends BaseTest {
+import ios.tronlink.com.tronlink.wallet.UITest.base.BaseTest;
+import ios.tronlink.com.tronlink.wallet.UITest.pages.AssetPage;
+import ios.tronlink.com.tronlink.wallet.UITest.pages.MinePage;
+import ios.tronlink.com.tronlink.wallet.UITest.pages.NodeSetPage;
+import ios.tronlink.com.tronlink.wallet.UITest.pages.SendTrxPage;
+import ios.tronlink.com.tronlink.wallet.UITest.pages.SettingPage;
+import ios.tronlink.com.tronlink.wallet.UITest.pages.TrxPage;
+import ios.tronlink.com.tronlink.wallet.utils.Helper;
 
+public class DappSendTrxTest extends BaseTest {
+    String successNumber;
     public SendTrxPage enterToSendTrxPage() throws Exception {
         AssetPage asset = new AssetPage(DRIVER);
         SendTrxPage transfer = asset.enterSendTrxPage();
@@ -62,19 +67,32 @@ public class DappChainSendTrxTest extends BaseTest {
         Assert.assertTrue( Helper.guaranteeDappChain(DRIVER));
     }
 
+    @Test(description = "SendTrx success test",alwaysRun = true)
+    public void test001_sendTrxSuccess() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        SendTrxPage transfer = asset.enterSendTrxPage();
+        String count = random(10,10);
+        log(count);
+        successNumber = count;
+        TrxPage tokenpage = transfer.sendTrxWithNumber(successNumber);
+        double trcBefore = Double.parseDouble(removeSymbol(tokenpage.trxTotal_text.getText()));
+        transfer.back_bt.click();//返回到首页资产页
+        waiteTime();
+        tokenpage = asset.enterTrxPage();
+        double trcafter = Double.parseDouble(removeSymbol(tokenpage.trxTotal_text.getText()));
 
-    @Test(description = "input Privatekey to Receiving address",alwaysRun = true)
-    public void tsst001_inputPrivatekey() throws Exception {
-        SendTrxPage transfer = enterToSendTrxPage();
-        transfer.sendKey(transfer.testfieldArray.get(1),"324a2052e491e99026442d81df4d2777292840c1b3949e20696c49096c6bacb0");
-        Helper.tapWhitePlace(transfer.driver);
-        Assert.assertTrue(Helper.contentTexts(transfer.alltextArray,"地址格式不正确"));
+        System.out.println(count   + "   " + trcBefore  + " " + trcafter);
+        Assert.assertTrue(trcafter + Integer.parseInt(removeSymbol(count)) <= trcBefore);
+
     }
 
 
 
+
+
+
     @Test(description = "input error address to Receiving address",alwaysRun = true)
-    public void tsst002_inputErrorAddress() throws Exception {
+    public void test002_inputErrorAddress() throws Exception {
         SendTrxPage transfer = enterToSendTrxPage();
         transfer.sendKey(transfer.testfieldArray.get(1),"TFjmzQrQrkUWbu2Qs5NWXjj1F4D3m8a");
         Helper.tapWhitePlace(transfer.driver);
@@ -83,7 +101,7 @@ public class DappChainSendTrxTest extends BaseTest {
 
 
     @Test(description = "input not active Receiving address",alwaysRun = true)
-    public void tsst003_inputNotActiveAddress() throws Exception {
+    public void test003_inputNotActiveAddress() throws Exception {
         SendTrxPage transfer = enterToSendTrxPage();
         transfer.sendKey(transfer.testfieldArray.get(1),"TFjmzQrQrkUWbu2Qs5NWXjj1F4D3m8aJvu");
         Helper.tapWhitePlace(transfer.driver);
@@ -95,7 +113,7 @@ public class DappChainSendTrxTest extends BaseTest {
 
     @Parameters({"address"})
     @Test(description = "input Receiving address same as send address",alwaysRun = true)
-    public void tsst004_inputReceivingAddressSameAsSend(String address) throws Exception {
+    public void test004_inputReceivingAddressSameAsSend(String address) throws Exception {
         SendTrxPage transfer = enterToSendTrxPage();
         transfer.sendKey(transfer.testfieldArray.get(1),address);
         Helper.tapWhitePlace(transfer.driver);
@@ -105,7 +123,7 @@ public class DappChainSendTrxTest extends BaseTest {
 
 
     @Test(description = "input Null Receiving address",alwaysRun = true)
-    public void tsst005_inputNullReceivingAddress() throws Exception {
+    public void test005_inputNullReceivingAddress() throws Exception {
         SendTrxPage transfer = enterToSendTrxPage();
         transfer.sendKey(transfer.testfieldArray.get(2),"1");
         Assert.assertFalse(transfer.findsend_btn().isEnabled()); //send btn can click
@@ -114,7 +132,7 @@ public class DappChainSendTrxTest extends BaseTest {
 
 
     @Test(description = "input max send number",alwaysRun = true)
-    public void tsst006_inputMaxSendNumber() throws Exception {
+    public void test006_inputMaxSendNumber() throws Exception {
         SendTrxPage transfer = enterToSendTrxPage();
         transfer.sendAllTrx("max");
         Helper.tapWhitePlace(transfer.driver);
@@ -125,7 +143,7 @@ public class DappChainSendTrxTest extends BaseTest {
 
 
     @Test(description = "input mix send number",alwaysRun = true)
-    public void tsst007_inputMixSendNumber() throws Exception {
+    public void test007_inputMixSendNumber() throws Exception {
         SendTrxPage transfer = enterToSendTrxPage();
         transfer.sendAllTrx("mix");
         Helper.tapWhitePlace(transfer.driver);
@@ -135,7 +153,7 @@ public class DappChainSendTrxTest extends BaseTest {
 
 
     @Test(description = "input too Much TRX send number",alwaysRun = true)
-    public void tsst008_inputTooMuchSendNumber() throws Exception {
+    public void test008_inputTooMuchSendNumber() throws Exception {
         SendTrxPage transfer = enterToSendTrxPage();
         transfer.sendAllTrx("tooMuch");
         Helper.tapWhitePlace(transfer.driver);
@@ -144,7 +162,7 @@ public class DappChainSendTrxTest extends BaseTest {
 
 
     @Test(description = "password error",alwaysRun = true)
-    public void tsst009_passwordError() throws Exception {
+    public void test009_passwordError() throws Exception {
         SendTrxPage transfer = enterToSendTrxPage();
         transfer.testfieldArray.get(1).sendKeys("TG5wFVvrJiTkBA1WaZN3pzyJDfkgHMnFrp");
         transfer.testfieldArray.get(2).sendKeys("1");
@@ -164,7 +182,7 @@ public class DappChainSendTrxTest extends BaseTest {
 
 
     @Test(description = "Receiving address trim",alwaysRun = true)
-    public void tsst010_receivingAddressTrim() throws Exception {
+    public void test010_receivingAddressTrim() throws Exception {
         SendTrxPage transfer = enterToSendTrxPage();
         transfer.testfieldArray.get(1).sendKeys("  " + "TG5wFVvrJiTkBA1WaZN3pzyJDfkgHMnFrp" + "  ");
         Helper.tapWhitePlace(transfer.driver);
@@ -176,7 +194,7 @@ public class DappChainSendTrxTest extends BaseTest {
 
 
     @Test(description = "Receiving Minimum Trx",alwaysRun = true)
-    public void tsst011_sendMinimumTrx() throws Exception {
+    public void test011_sendMinimumTrx() throws Exception {
         SendTrxPage transfer = enterToSendTrxPage();
         TimeUnit.SECONDS.sleep(1);
         transfer.testfieldArray.get(1).sendKeys("TG5wFVvrJiTkBA1WaZN3pzyJDfkgHMnFrp");
@@ -193,36 +211,11 @@ public class DappChainSendTrxTest extends BaseTest {
 
 
 
-    @Test(description = "SendTrx success test",alwaysRun = true)
-    public void tsst012_sendTrxSuccess() throws Exception {
-        AssetPage asset = new AssetPage(DRIVER);
-        SendTrxPage transfer = asset.enterSendTrxPage();
-        transfer.testfieldArray.get(1).sendKeys("TG5wFVvrJiTkBA1WaZN3pzyJDfkgHMnFrp");
-        transfer.testfieldArray.get(2).sendKeys("1");
-        Helper.tapWhitePlace(transfer.driver);
-        TimeUnit.SECONDS.sleep(2);
-        transfer.send_btn.click();
-        TimeUnit.SECONDS.sleep(1);
-        transfer.transferNow_btn.click();
-        TimeUnit.SECONDS.sleep(1);
-        transfer.InputPasswordConfim_btn.sendKeys("Test0001");
-        transfer.broadcastButtonClick();
-        TimeUnit.SECONDS.sleep(4);
-        TrxPage tokenpage = new TrxPage(transfer.driver);
-        double trcBefore = Double.parseDouble(removeSymbol(tokenpage.trxTotal_text.getText()));
-        transfer.back_bt.click();//返回到首页资产页
-        AssetPage assetpage = new AssetPage(DRIVER);
-        tokenpage = assetpage.enterTrxPage();
-        TimeUnit.SECONDS.sleep(2);
-        double trcafter = Double.parseDouble(removeSymbol(tokenpage.trxTotal_text.getText()));
-        System.out.println("old:" + trcBefore + "\nnew:" + trcafter);
-        Assert.assertTrue(trcafter + 1 == trcBefore);
-    }
 
 
 
     @Test(description = "Receiving Minimum Extra Trx",alwaysRun = true)
-    public void tsst013_sendMinimumTrx() throws Exception {
+    public void test012_sendMinimumTrx() throws Exception {
         SendTrxPage transfer = enterToSendTrxPage();
         transfer.testfieldArray.get(1).sendKeys("TG5wFVvrJiTkBA1WaZN3pzyJDfkgHMnFrp");
         transfer.testfieldArray.get(2).sendKeys("0.0000001");
@@ -231,4 +224,22 @@ public class DappChainSendTrxTest extends BaseTest {
         Assert.assertTrue(Helper.contentTexts(transfer.alltextArray,"0.000001"));
 
     }
+
+    @Test(description = "input Privatekey to Receiving address",alwaysRun = true)
+    public void test013_inputPrivatekey() throws Exception {
+        SendTrxPage transfer = enterToSendTrxPage();
+        transfer.sendKey(transfer.testfieldArray.get(1),"324a2052e491e99026442d81df4d2777292840c1b3949e20696c49096c6bacb0");
+        Helper.tapWhitePlace(transfer.driver);
+        Assert.assertTrue(Helper.contentTexts(transfer.alltextArray,"地址格式不正确"));
+    }
+
+    @Test(description = "Check OutNumberInRecord Trx",alwaysRun = true)
+    public void test014_CheckOutNumberInRecordTrx() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        TrxPage page = asset.enterTrxPage();
+        String findString = "-" + successNumber;
+        Assert.assertTrue(page.enterNumberRecordPage(findString));
+
+    }
+
 }
