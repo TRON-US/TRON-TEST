864ddebcd7130d1c9fdeeff63183d19593cd43c2
pangpangfeng <7577288@qq.com>
2020-01-22 11:46:42 +0800

    Add cold wallet framework

---
+++ src/test/java/ios/tronlink/com/tronlink/wallet/regression/ColdWalletTest.java
@@ -0,0 +1,23 @@
+package ios.tronlink.com.tronlink.wallet.regression;
+
+
+import ios.tronlink.com.tronlink.wallet.UITest.base.ColdBaseTest;
+import ios.tronlink.com.tronlink.wallet.UITest.pages.AssetPage;
+import ios.tronlink.com.tronlink.wallet.UITest.pages.MinePage;
+import ios.tronlink.com.tronlink.wallet.utils.Helper;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ * 关于我们功能测试
+ */
+public class ColdWalletTest extends ColdBaseTest {
+
+    @Test(description = "test AboutUs assert",alwaysRun = true)
+    public void test001_aboutUs() throws Exception {
+        AssetPage assetPage = new AssetPage(DRIVER);
+    }
+
+
+}
