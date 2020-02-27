dea537efc259ef755037d417364b6fc638c08fff
pangpangfeng <7577288@qq.com>
2020-02-27 18:25:41 +0800

    Move android dapp test case to dir 'dappChain'

---
+++ src/test/java/android/com/tronlink/wallet/dappChain/DappNetWithdraw20.java
@@ -0,0 +1,170 @@
+package android.com.tronlink.wallet.dappChain;
+
+import android.com.wallet.UITest.base.Base;
+import android.com.wallet.pages.AssetPage;
+import android.com.wallet.pages.MinePage;
+import android.com.wallet.pages.NodeSetPage;
+import android.com.wallet.pages.SettingPage;
+import android.com.wallet.pages.TransferPage;
+import android.com.wallet.pages.TrxPage;
+
+import java.util.Random;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import android.com.utils.Helper;
+
+public class DappNetWithdraw20 extends Base {
+    Random rand = new Random();
+    float withdrawTrc20Amount;
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownAfterClass() {
+        //reset DAPP chain trun main chain
+        changeToMainChain();
+        try {
+            DRIVER.quit();
+        } catch (Exception e) {
+        }
+    }
+
+
+    @Parameters({"privateKey"})
+    @BeforeClass(alwaysRun = true)
+    public void setUpBefore(String privateKey) throws Exception {
+        new Helper().getSign(privateKey, DRIVER);
+        changeToDappChain();
+        DRIVER.closeApp();
+        DRIVER.activateApp("com.tronlink.wallet");
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void afterMethod() {
+        try {
+            DRIVER.closeApp();
+            DRIVER.activateApp("com.tronlink.wallet");
+            TimeUnit.SECONDS.sleep(1);
+        }catch (Exception e){}
+    }
+
+
+    //reset app turn to MainChain
+    public void changeToMainChain() {
+        try {
+            SettingPage set = enterSettingPage();
+            NodeSetPage nodeSet = set.enterNodeSetPage();
+            nodeSet.enterSettingPageChoiseMainChain();
+            TimeUnit.SECONDS.sleep(1);
+        } catch (Exception e) {
+        }
+    }
+
+    public void changeToDappChain() {
+        try {
+            SettingPage set = enterSettingPage();
+            NodeSetPage nodeSet = set.enterNodeSetPage();
+            nodeSet.enterSettingPageChoiseDappChain();
+            TimeUnit.SECONDS.sleep(1);
+        } catch (Exception e) {
+        }
+    }
+
+
+    //enter SettingPage
+    public SettingPage enterSettingPage() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        MinePage mine = asset.enterMinePage();
+        return mine.enterSettingPage();
+    }
+
+
+    //enter TRXPage
+    public TrxPage enterTrc20Page() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        return asset.enterTrx20Page();
+    }
+
+
+    @Test(enabled = true,description = "Check withdraw from dapp chain information", alwaysRun = true)
+    public void test001_checkTransferOutHits() throws Exception {
+        TrxPage trx = enterTrc20Page();
+        TransferPage transferOut = trx.enterTransferPage();
+        String info = transferOut.getTransferInfo("hits");
+        Assert.assertTrue(info.contains("转出需要执行智能合约。执行智能合约同时会消耗能量") || info.contains("requires the execution of a smart contract"));
+    }
+
+
+    @Test(enabled = true,description = "Check withdraw from dapp chain fee", alwaysRun = true)
+    public void test002_checkTransferOutFee() throws Exception {
+        TrxPage trx = enterTrc20Page();
+        TransferPage transferOut = trx.enterTransferPage();
+        String info = transferOut.getTransferInfo("fee");
+        int count = Integer.valueOf(info);
+        Assert.assertTrue(50 <= count && count <= 500);
+    }
+
+
+    @Test(enabled = true,description = "Check Available Balance", alwaysRun = true)
+    public void test003_checkAvailableBalance() throws Exception {
+        SettingPage set = enterSettingPage();
+        NodeSetPage nodeSet = set.enterNodeSetPage();
+        set = nodeSet.enterSettingPageChoiseMainChain();
+        MinePage mine = set.enterMinePage();
+        AssetPage asset = mine.enterAssetPage();
+        int trxCount = Integer.valueOf(removeSymbol(asset.getTrxCount()));
+        TrxPage trx = asset.enterTrxPage();
+        int frozenCount = Integer.valueOf(removeSymbol(trx.freezeCount_text.getText()));
+        TransferPage transferOut = trx.enterTransferPage();
+        int availableBalance = Integer.valueOf(removeSymbol(transferOut.availableBalance_text.getText().split(" ")[1]));
+        System.out.println("trxCount" + trxCount);
+        System.out.println("frozenCount" + frozenCount);
+        System.out.println("availableBalance" + availableBalance);
+        Assert.assertTrue(trxCount - (frozenCount + availableBalance) >= -1
+            && trxCount - (frozenCount + availableBalance) <= 1);
+    }
+
+
+    @Test(enabled = true,description = "Withdraw from dapp chain success and checkout available trx", alwaysRun = true)
+    public void test004_checkAvailableBalance() throws Exception {
+        TrxPage trx = enterTrc20Page();
+        int trxCount = Integer.valueOf(removeSymbol(trx.trxTotal_text.getText()));
+        System.out.println("trxCount = " + trxCount);
+        TransferPage transferOut = trx.enterTransferPage();
+        withdrawTrc20Amount = rand.nextFloat() + 1;
+        trx = transferOut.enterTrxPageWithTransferSuccess(Float.toString(withdrawTrc20Amount));
+        int trxCountNow = Integer.valueOf(removeSymbol(trx.trxTotal_text.getText()));
+        System.out.println("trxCountNow = " + trxCountNow);
+        Assert.assertTrue(trxCount >= trxCountNow);
+    }
+
+
+    @Test(enabled = true,description = "Withdraw trc20 from dapp chain Recording")
+    public void test005_transferOutSuccessRecording() throws Exception {
+        TrxPage trx = enterTrc20Page();
+        int tries = 0;
+        Boolean exist = false;
+        while (exist == false && tries++ < 5) {
+            try {
+                AssetPage arret = trx.enterAssetPage();
+                trx = arret.enterTrx20Page();
+                trx.tranfer_tab.get(3).click();
+                System.out.println(trx.tranferIncount_text.get(1).getText());
+                String tranferInCount = trx.tranferIncount_text.get(1).getText().split(" ")[1];
+                if (Float.toString(withdrawTrc20Amount).substring(0, 5)
+                    .equals(tranferInCount.substring(0, 5))) {
+                    exist = true;
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+        Assert.assertTrue(exist);
+    }
+}
---
+++ src/test/java/android/com/tronlink/wallet/dappChain/MainNetDeposit.java
@@ -0,0 +1,163 @@
+package android.com.tronlink.wallet.dappChain;
+
+import java.util.Random;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import android.com.utils.Helper;
+import android.com.wallet.UITest.base.Base;
+import android.com.wallet.pages.AssetPage;
+import android.com.wallet.pages.MinePage;
+import android.com.wallet.pages.NodeSetPage;
+import android.com.wallet.pages.SettingPage;
+import android.com.wallet.pages.TransferPage;
+import android.com.wallet.pages.TrxPage;
+
+
+public class MainNetDeposit extends Base {
+    Random rand = new Random();
+    float depositTrxAmount;
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownAfterClass() {
+        //Base.tearDownAfterClass();
+        try {
+            DRIVER.quit();
+        } catch (Exception e) {
+        }
+    }
+
+
+    @Parameters({"privateKey"})
+    @BeforeClass(alwaysRun = true)
+    public void setUpBefore(String privateKey) throws Exception {
+        new Helper().getSign(privateKey, DRIVER);
+    }
+
+
+    @AfterMethod(alwaysRun = true)
+    public void afterMethod() {
+        try {
+            DRIVER.closeApp();
+            DRIVER.activateApp("com.tronlink.wallet");
+        }catch (Exception e){}
+    }
+
+
+    //enter SettingPage
+    public SettingPage enterSettingPage() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        MinePage mine = asset.enterMinePage();
+        return mine.enterSettingPage();
+    }
+
+
+    //enter TRXPage
+    public TrxPage enterTrxPage() throws Exception {
+        SettingPage set = enterSettingPage();
+        NodeSetPage nodeSet = set.enterNodeSetPage();
+        set = nodeSet.enterSettingPageChoiseMainChain();
+        MinePage mine = set.enterMinePage();
+        AssetPage asset = mine.enterAssetPage();
+        return asset.enterTrxPage();
+    }
+
+    @Test(enabled = true,description = "Check TransferIn Chain Name", alwaysRun = true)
+    public void test002_checkTransferInChainName() throws Exception {
+        TrxPage trx = enterTrxPage();
+        TransferPage transferIn = trx.enterTransferPage();
+        String chain = transferIn.chain_text.getText();
+        Assert.assertTrue(chain.equals("DAppChain"));
+    }
+
+
+    @Test(enabled = true,description = "Check TransferIn Trx Count", alwaysRun = true)
+    public void test003_checkTransferInTrx() throws Exception {
+        TrxPage trx = enterTrxPage();
+        TransferPage transferIn = trx.enterTransferPage();
+        String info = transferIn.getTransferInfo("trx");
+        Assert.assertTrue(info.contains("10"));
+    }
+
+
+    @Test(enabled = true,description = "Check TransferIn Hits", alwaysRun = true)
+    public void test004_checkTransferInHits() throws Exception {
+        TrxPage trx = enterTrxPage();
+        TransferPage transferIn = trx.enterTransferPage();
+        String info = transferIn.getTransferInfo("hits");
+        System.out.println("info:" + info);
+        Assert.assertTrue(info.contains("转入需要执行智能合约。执行智能合约同时会消耗能量") || info.equals("转入需要执行智能合约。执行智能合约同时会消耗 Energy。") || info.contains("requires the execution of a smart contract"));
+    }
+
+
+    @Test(enabled = true,description = "Check TransferIn Fee", alwaysRun = true)
+    public void test005_checkTransferInFee() throws Exception {
+        TrxPage trx = enterTrxPage();
+        TransferPage transferIn = trx.enterTransferPage();
+        String info = transferIn.getTransferInfo("fee");
+        int count = Integer.valueOf(info);
+        Assert.assertTrue(50 <= count && count <= 500);
+    }
+
+
+    @Test(enabled = true,description = "Check Available Balance", alwaysRun = true)
+    public void test006_checkAvailableBalance() throws Exception {
+        SettingPage set = enterSettingPage();
+        NodeSetPage nodeSet = set.enterNodeSetPage();
+        set = nodeSet.enterSettingPageChoiseMainChain();
+        MinePage mine = set.enterMinePage();
+        AssetPage asset = mine.enterAssetPage();
+        int trxCount = Integer.valueOf(removeSymbol(asset.getTrxCount()));
+        System.out.println("trxCount = " + trxCount);
+        TrxPage trx = asset.enterTrxPage();
+        int frozenCount = Integer.valueOf(removeSymbol(trx.freezeCount_text.getText()));
+        System.out.println("frozenCount = " + frozenCount);
+        TransferPage transferIn = trx.enterTransferPage();
+        int availableBalance = Integer.valueOf(removeSymbol(transferIn.availableBalance_text.getText().split(" ")[1]));
+        System.out.println("availableBalance = " + availableBalance);
+        Assert.assertTrue(trxCount == frozenCount + availableBalance);
+    }
+
+
+    @Test(enabled = true,description = "Deposit trx into Dapp chain success checkout available trx", alwaysRun = true)
+    public void test007_depositTrxIntoDappChain() throws Exception {
+        TrxPage trx = enterTrxPage();
+        depositTrxAmount = rand.nextFloat() + 10;
+        TransferPage transferIn = trx.enterTransferPage();
+        transferIn.enterTrxPageWithTransferSuccess(Float.toString(depositTrxAmount));
+    }
+
+    @Test(enabled = true,description = "Trc10 deposit into Dapp chain Success Recording", alwaysRun = true)
+    public void test009_depositSuccessRecording() throws Exception {
+        TrxPage trx = enterTrxPage();
+        int tries = 0;
+        Boolean exist = false;
+        while (exist == false && tries++ < 5) {
+            try {
+                AssetPage arret = trx.enterAssetPage();
+                trx = arret.enterTrxPage();
+                trx.tranfer_tab.get(3).click();
+                String tranferInCount = trx.tranferIncount_text.get(1).getText().split(" ")[1];
+                System.out.println("dappChainSendTrc10Amount:" + depositTrxAmount);
+                System.out.println("tranferInCount:" + tranferInCount);
+                if (Float.toString(depositTrxAmount).substring(0, 5)
+                    .equals(tranferInCount.substring(0, 5))) {
+                    exist = true;
+                    break;
+                }
+            } catch (Exception e) {
+            }
+        }
+        Assert.assertTrue(exist);
+    }
+
+
+
+
+}
