551a2cc850d939a3db4bb1c431d3a77832ab07a6
hechuan <hccgk@sina.com>
2020-06-28 11:18:11 +0800

    trc20token

--- src/test/java/ios/tronlink/com/tronlink/wallet/UITest/pages/SendTrxPage.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/UITest/pages/SendTrxPage.java
@@ -111,7 +111,6 @@ public class SendTrxPage extends AbstractPage {
 
 //shieldedCurrentBalance  余额  shieldedLimitHelpBtn  限额按钮  shieldedFeeLabel 手续费   shieldedLimitLabel 单笔限额
 
-
     public WebElement getTrc20Token() throws Exception{
         waiteTime();
 //        List<WebElement> cells =  driver.findElementByClassName("XCUIElementTypeTable").findElements(By.className("XCUIElementTypeCell"));
@@ -131,12 +130,12 @@ public class SendTrxPage extends AbstractPage {
 
         } catch (Exception e) {
             System.out.println("no DappChain");
-
+            return driver.findElementById("TRX (TXkdXbzjoLpxGAD2strP1zwjJzR6osNfD7)");
 
         }
 
-
     }
+
     public WebElement getTrc10Token() throws Exception{
         waiteTime();
 //        List<WebElement> cells =  driver.findElementByClassName("XCUIElementTypeTable").findElements(By.className("XCUIElementTypeCell"));
