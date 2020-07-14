12a9977e6c94a5d93219417bdd33b45a62a2095f
hechuan <hccgk@sina.com>
2020-07-14 11:09:39 +0800

    adjust tips

--- src/test/java/android/com/tronlink/wallet/shieldTransaction/ShieldSynTest.java
+++ src/test/java/android/com/tronlink/wallet/shieldTransaction/ShieldSynTest.java
@@ -89,7 +89,7 @@ public class ShieldSynTest extends Base {
         SendTrzPage transfer = enterToSendTrzPage();
         transfer.sendKey(transfer.receiveAddress_text, shieldAddress);
         String hits = transfer.formatErrorHits_text.getText();
-        Assert.assertTrue(hits.equals("转出地址与接收地址不能相同") || hits.equals("发送地址与接收地址不能相同") || hits.contains("cannot be the same"));
+        Assert.assertTrue(hits.equals("转出账户与接收账户不能相同") || hits.equals("发送账户与接收账户不能相同") || hits.contains("cannot be the same"));
     }
 
     @Test(enabled = true,description = "Shield to shield transaction record test")
