62c12d6dec24dd3e7aa7d41024a4ff5a7dad4586
hechuan <hccgk@sina.com>
2020-08-06 16:42:48 +0800

    add two test case in asset page

--- src/test/java/android/com/tronlink/wallet/regression/AssetsPageTest.java
+++ src/test/java/android/com/tronlink/wallet/regression/AssetsPageTest.java
@@ -4,10 +4,14 @@ package android.com.tronlink.wallet.regression;
 import android.com.utils.Helper;
 import android.com.wallet.UITest.base.Base;
 
+import android.com.wallet.pages.AssetPage;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * 资产页面测试
@@ -50,6 +54,30 @@ public class AssetsPageTest extends Base {
         } catch (Exception e) {
         }
     }
+
+    @Test(enabled = true,description = "assets total amount test", alwaysRun = true)
+    public void test001_AssetsTotalAmountTest() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        TimeUnit.SECONDS.sleep(3);
+        String about = asset.trxtotalvalue.getText();
+        String result = sepLeftNumberTextToString(about,"TRX").trim();
+        Double number = Double.parseDouble(result);
+        System.out.println("Total number:" + number);
+        Assert.assertTrue(number > 0 );
+    }
+
+    @Test(enabled = true,description = "assets total about amount test", alwaysRun = true)
+    public void test002_AssetsTotalAboutAmountTest() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        TimeUnit.SECONDS.sleep(3);
+        String about = asset.abountmoneyvalue.getText();
+        String result = sepLeftNumberTextToString(about,"USD").replace("≈"," ").trim();
+        Double number = Double.parseDouble(result);
+        System.out.println("Total about number:" + number);
+        Assert.assertTrue(number > 0 );
+    }
+
+
 
 
 }
