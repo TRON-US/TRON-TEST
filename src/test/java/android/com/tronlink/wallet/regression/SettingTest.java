8b130c5107a1313e06fefb594653535392587b38
hechuan <hccgk@sina.com>
2020-10-12 19:16:01 +0800

    adjust setting DApp测试工具

--- src/test/java/android/com/tronlink/wallet/regression/SettingTest.java
+++ src/test/java/android/com/tronlink/wallet/regression/SettingTest.java
@@ -74,7 +74,7 @@ public class SettingTest extends Base {
         MinePage mine = asset.enterMinePage();
         DAPP_BrowerPage dapp = mine.enterDAPP_BrowerPage();
         dapp.testUrl();
-        Assert.assertEquals("TEST", dapp.dappTtile_btn.getText());
+        Assert.assertEquals("DApp测试工具", dapp.dappTtile_btn.getText());
     }
 
     @Test(description = "choose DAppChain", alwaysRun = true)
