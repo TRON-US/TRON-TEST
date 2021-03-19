f93ab680505d17d75fe5dcc22ca3c7b31e8bd6ac
hechuan <hccgk@sina.com>
2021-03-19 18:48:58 +0800

    addasset in android

--- src/test/java/android/com/tronlink/wallet/regression/AddAssetsTest.java
+++ src/test/java/android/com/tronlink/wallet/regression/AddAssetsTest.java
@@ -1,9 +1,12 @@
 package android.com.tronlink.wallet.regression;
 
+import android.com.utils.Configuration;
 import android.com.utils.Helper;
 import android.com.wallet.UITest.base.Base;
 import android.com.wallet.pages.AddAssertPage;
+import android.com.wallet.pages.AddressBookPage;
 import android.com.wallet.pages.AssetPage;
+import android.com.wallet.pages.MinePage;
 import android.com.wallet.pages.SearchAssertPage;
 
 import org.testng.Assert;
@@ -13,6 +16,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -44,14 +48,13 @@ public class AddAssetsTest extends Base {
         }
     }
 
-
     @Test(description = "Add Asset Page Tip Test",alwaysRun = true)
     public void test001_AddAssetPageTipTest() throws Exception{
         AssetPage asset = new AssetPage(DRIVER);
         AddAssertPage page = asset.enterAddAssertPage();
-        Assert.assertTrue(page.tipview.getText().contains("当有新的资产时，可进入查看"));
+        Assert.assertTrue(page.tipview.getText().contains("可移除不需要展示的资产"));
         page.close.click();
-        Assert.assertTrue(page.et_search.getText().contains("输入通证名称/通证 ID/智能合约地址"));
+        Assert.assertTrue(page.et_search.getText().contains("输入名称/ID/合约地址"));
         Assert.assertTrue(page.iv_search_icon.isEnabled());
         Assert.assertTrue(page.firstTab.getText().contains("首页资产管理"));
         Assert.assertTrue(page.secondTab.getText().contains("我的全部资产"));
@@ -60,6 +63,76 @@ public class AddAssetsTest extends Base {
         Assert.assertTrue(page.accountnumbers.size() > 1);
 
     }
+
+
+//    @Test(description = "Add Asset Page Tip Test",alwaysRun = true)
+//    public void test000_addressbook30Test() throws Exception{
+//        AssetPage asset = new AssetPage(DRIVER);
+//        MinePage page = asset.enterMinePage();
+//        AddressBookPage addressBookPage = page.enterAddressBookPage();
+//
+//        int book = 11;
+//        while (book < 11){
+//            log("iosMultiSignAccount.owner"+String.valueOf(book)+"Address");
+//
+//            String addressString =  Configuration.getByPath("testng.conf")
+//                    .getString("androidMultiSignAccount.multiSign"+String.valueOf(book)+"Address");
+//            log("find address: " + addressString);
+//
+//            addressBookPage.addAddressBook_btn.click();
+//            TimeUnit.SECONDS.sleep(2);
+//
+//            Random rand = new Random();
+//            String addressName = "address-" + String.valueOf(book);
+//            addressBookPage.addName_input.sendKeys(addressName);
+//            addressBookPage.addAddress_input.sendKeys(addressString);
+////            addressBookPage.addNote_input.sendKeys(addressString);
+//            addressBookPage.save_btn.click();
+//            book++;
+//        }
+//
+//        while (book < 21){
+//            log("iosMultiSignAccount.owner"+String.valueOf(book)+"Address");
+//
+//                        String addressString = Configuration.getByPath("testng.conf")
+//                    .getString("iosMultiSignAccount.multiSign" + String.valueOf(book-10) + "Address");
+//            log("find address: " + addressString);
+//
+//            addressBookPage.addAddressBook_btn.click();
+//            TimeUnit.SECONDS.sleep(2);
+//
+//            Random rand = new Random();
+//            String addressName = "address-" + String.valueOf(book);
+//            addressBookPage.addName_input.sendKeys(addressName);
+//            addressBookPage.addAddress_input.sendKeys(addressString);
+////            addressBookPage.addNote_input.sendKeys(addressString);
+//            addressBookPage.save_btn.click();
+//            book++;
+//        }
+//
+//        while (book < 31){
+//            log("iosMultiSignAccount.owner"+String.valueOf(book)+"Address");
+//
+//            String addressString = Configuration.getByPath("testng.conf")
+//                    .getString("iosMultiSignAccount.owner" + String.valueOf(book-20) + "Address");
+//            log("find address: " + addressString);
+//
+//            addressBookPage.addAddressBook_btn.click();
+//            TimeUnit.SECONDS.sleep(2);
+//
+//            Random rand = new Random();
+//            String addressName = "address-" +String.valueOf(book);
+//            addressBookPage.addName_input.sendKeys(addressName);
+//            addressBookPage.addAddress_input.sendKeys(addressString);
+////            addressBookPage.addNote_input.sendKeys(addressString);
+//            addressBookPage.save_btn.click();
+//            book++;
+//        }
+//
+//    }
+
+
+
 
 
 }
