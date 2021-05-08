0adaf2a79f616d36010b9cbc234d25ee60b3348e
hechuan <hccgk@sina.com>
2021-05-08 14:37:48 +0800

    sendtrc20 in ios v4.1

--- src/test/java/ios/tronlink/com/tronlink/wallet/regression/SendTrc20.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/regression/SendTrc20.java
@@ -91,9 +91,7 @@ public class SendTrc20 extends BaseTest {
         transfer.testfieldArray.get(1).sendKeys("TG5wFVvrJiTkBA1WaZN3pzyJDfkgHMnFrp");
         Helper.tapWhitePlace(transfer.driver);
         waiteTime();
-        transfer.token_btn.click();
-        waiteTime();
-        transfer.clickOffsetElement(transfer.getTrc20Token());
+        transfer.selectToken20TRX();
         waiteTime();
         transfer.testfieldArray.get(2).sendKeys("1");
         TimeUnit.SECONDS.sleep(2);
