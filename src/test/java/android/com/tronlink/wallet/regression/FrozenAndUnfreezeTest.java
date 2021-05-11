23ebe1b990ba72dfb0e7fcbda4665568974e0e8c
hechuan <hccgk@sina.com>
2021-05-11 15:25:00 +0800

    try by id in deep xml in android

--- src/test/java/android/com/tronlink/wallet/regression/FrozenAndUnfreezeTest.java
+++ src/test/java/android/com/tronlink/wallet/regression/FrozenAndUnfreezeTest.java
@@ -244,7 +244,7 @@ public class FrozenAndUnfreezeTest extends Base {
         MinePage mine = asset.enterMinePage();
         TransactionRecordPage transaction = mine.enterTransactionRecordPage();
         String transactionType = transaction.transactionRecords.get(0).findElement(By.id("com.tronlinkpro.wallet:id/tv_contract_title")).getText();
-        String resourceType = transaction.transactionRecords.get(0).findElement(By.id("com.tronlinkpro.wallet:id/tv_two")).getText();
+        String resourceType = transaction.driver.findElement(By.id("com.tronlinkpro.wallet:id/tv_two")).getText();
         System.out.println(transactionType);
         Assert.assertTrue(transactionType.equals("冻结资产") || transactionType.equals("Freeze Asset"));
         Assert.assertTrue(resourceType.contains("BANDWIDTH"));
