2ee5ba5f8892c56bccbd52f24cc2a97116b8711e
hechuan <hccgk@sina.com>
2021-06-10 11:44:45 +0800

    more time for wait ad view

--- src/test/java/ios/tronlink/com/tronlink/wallet/UITest/pages/AssetPage.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/UITest/pages/AssetPage.java
@@ -19,7 +19,7 @@ public class AssetPage extends AbstractPage {
     public AssetPage(IOSDriver<?> driver) {
         super(driver);
         this.driver = driver;
-        driver.manage().timeouts().implicitlyWait(1,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);
         try {
             if (ad_pic.isDisplayed()) {
                 adClose_btn.click();
