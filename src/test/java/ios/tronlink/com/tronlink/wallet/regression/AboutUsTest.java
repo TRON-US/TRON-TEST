18655be305cb30d08d9f59b25e6432f9867a5800
hechuan <hccgk@sina.com>
2021-10-25 16:15:38 +0800

    ios version update

--- src/test/java/ios/tronlink/com/tronlink/wallet/regression/AboutUsTest.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/regression/AboutUsTest.java
@@ -23,9 +23,9 @@ public class AboutUsTest extends BaseTest {
 
 
 
-
     @Test(description = "test AboutUs test",alwaysRun = true)
     public void test001_aboutUs() throws Exception {
+
         AssetPage assetPage = new AssetPage(DRIVER);
         MinePage minePage =  assetPage.enterMinePage();
         minePage.enterAboutUsPage();
@@ -37,14 +37,15 @@ public class AboutUsTest extends BaseTest {
     public void test002_versionTest() throws Exception {
         AssetPage assetPage = new AssetPage(DRIVER);
         MinePage minePage =  assetPage.enterMinePage();
-        Assert.assertTrue(Helper.isElementExist(assetPage.driver,"v4.3.0"));
+        Helper.swipScreenLitter(minePage.driver);
+        Assert.assertTrue(Helper.isElementExist(assetPage.driver,"v4.4.0"));
         minePage.enterAboutUsPage();
         Assert.assertTrue(Helper.isElementExist(assetPage.driver,"版本日志"));
         Assert.assertTrue(Helper.isElementExist(assetPage.driver,"用户协议"));
         minePage.versionNote_btn.click();
         TimeUnit.SECONDS.sleep(5);
         waiteTime();
-        Assert.assertTrue(Helper.isElementExist(assetPage.driver,"4.2.4"));
+        Assert.assertTrue(Helper.isElementExist(assetPage.driver,"4.3.1"));
     }
 
     @Test(description = "test versionUpdate test",alwaysRun = true)
