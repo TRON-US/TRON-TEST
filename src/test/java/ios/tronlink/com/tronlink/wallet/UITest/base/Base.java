3ac5fd48fc0809c14bad7c1cfa15ab54e5fb6ed9
hechuan <hccgk@sina.com>
2021-11-07 22:24:13 +0800

    time reduce in ios

--- src/test/java/ios/tronlink/com/tronlink/wallet/UITest/base/Base.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/UITest/base/Base.java
@@ -37,7 +37,7 @@ public class Base {
 
     private SimpleDateFormat timeStamp = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss ");
 
-    public int RetryAgainTimes = 5;
+    public int RetryAgainTimes = 2;
 
     protected DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 
