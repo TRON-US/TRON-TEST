b1ae83f1a7c5ed450ab242e1d5466cbcc015944d
hechuan <hccgk@sina.com>
2021-12-14 14:38:24 +0800

    ios update about us

--- src/test/java/ios/tronlink/com/tronlink/wallet/regression/AboutUsTest.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/regression/AboutUsTest.java
@@ -22,38 +22,22 @@ import java.util.concurrent.TimeUnit;
 public class AboutUsTest extends BaseTest {
 
 
-
-    @Test(description = "test AboutUs test",alwaysRun = true)
-    public void test001_aboutUs() throws Exception {
-
-        AssetPage assetPage = new AssetPage(DRIVER);
-        MinePage minePage =  assetPage.enterMinePage();
-        minePage.enterAboutUsPage();
-        Assert.assertTrue(Helper.isElementExist(assetPage.driver,"关于我们"));
-
-    }
-
     @Test(description = "test version test",alwaysRun = true)
     public void test002_versionTest() throws Exception {
         AssetPage assetPage = new AssetPage(DRIVER);
         MinePage minePage =  assetPage.enterMinePage();
         Helper.swipScreenLitter(minePage.driver);
-        Assert.assertTrue(Helper.isElementExist(assetPage.driver,"v4.4.0"));
+        Assert.assertTrue(Helper.isElementExist(assetPage.driver,"v4.4.4"));
         minePage.enterAboutUsPage();
+        Assert.assertTrue(Helper.isElementExist(assetPage.driver,"关于我们"));
         Assert.assertTrue(Helper.isElementExist(assetPage.driver,"版本日志"));
+        Assert.assertTrue(Helper.isElementExist(assetPage.driver,"版本更新"));
         Assert.assertTrue(Helper.isElementExist(assetPage.driver,"用户协议"));
+        Assert.assertTrue(Helper.isElementExist(assetPage.driver,"加入社群"));
         minePage.versionNote_btn.click();
         TimeUnit.SECONDS.sleep(5);
         waiteTime();
-        Assert.assertTrue(Helper.isElementExist(assetPage.driver,"4.3.1"));
-    }
-
-    @Test(description = "test versionUpdate test",alwaysRun = true)
-    public void test003_versionUpdateTest() throws Exception {
-        AssetPage assetPage = new AssetPage(DRIVER);
-        MinePage minePage =  assetPage.enterMinePage();
-        minePage.enterAboutUsPage();
-        Assert.assertTrue(Helper.isElementExist(assetPage.driver,"版本更新"));
+        Assert.assertTrue(Helper.isElementExist(assetPage.driver,"4.4.4"));
     }
 
     @Test(description = "test enterGroup test" ,alwaysRun = true)
@@ -61,14 +45,12 @@ public class AboutUsTest extends BaseTest {
         AssetPage assetPage = new AssetPage(DRIVER);
         MinePage minePage =  assetPage.enterMinePage();
         minePage.enterAboutUsPage();
-        Assert.assertTrue(Helper.isElementExist(assetPage.driver,"加入社群"));
         minePage.intoGroup_btn.click();
         TimeUnit.SECONDS.sleep(5);
         waiteTime();
         Assert.assertTrue(Helper.isElementExist(assetPage.driver,"Twitter"));
         Assert.assertTrue(Helper.isElementExist(assetPage.driver,"微信"));
         Assert.assertTrue(Helper.isElementExist(assetPage.driver,"Telegram电报群"));
-
     }
 
 
