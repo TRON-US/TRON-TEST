e088cdb82a46abdd56506024eb01e9ff0cdeea75
hechuan <hccgk@sina.com>
2022-05-19 11:40:29 +0800

    receipt in A

--- src/test/java/android/com/tronlink/wallet/regression/ReceiptTest.java
+++ src/test/java/android/com/tronlink/wallet/regression/ReceiptTest.java
@@ -62,6 +62,7 @@ public class ReceiptTest extends Base {
      public void test002_watchWalletTitle() throws Exception {
          AssetPage asset = new AssetPage(DRIVER);
          asset.addWatchWallet("THmxT8BKeXSKd4Zu4HJN3hXPVC1xiNhEnX");
+         TimeUnit.SECONDS.sleep(3);
          ReceiptPage receiptPage = asset.enterReceiptPage();
          Assert.assertEquals(receiptPage.tv_address.getText(), "THmxT8BKeXSKd4Zu4HJN3hXPVC1xiNhEnX");
          Assert.assertTrue(receiptPage.isElementExist("com.tronlinkpro.wallet:id/qr"));
