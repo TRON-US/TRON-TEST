6a55429fad2e2a8c2b8bf6ee05e1301d7f121264
hechuan <hccgk@sina.com>
2022-06-09 10:34:04 +0800

    iOS retry times --

--- src/test/java/ios/tronlink/com/tronlink/wallet/UITest/base/Base.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/UITest/base/Base.java
@@ -39,7 +39,7 @@ public class Base {
 
     private SimpleDateFormat timeStamp = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss ");
 
-    public int RetryAgainTimes = 3;
+    public int RetryAgainTimes = 1;
 
     protected DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 
