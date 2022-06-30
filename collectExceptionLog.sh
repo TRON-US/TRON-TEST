c871ff6dd7feb2255743fb5e7e25bf4064bac175
hechuan <hccgk@sina.com>
2022-06-30 11:46:31 +0800

    Log collect in A

--- collectExceptionLog.sh
+++ collectExceptionLog.sh
@@ -1,6 +1,8 @@
 sleep 5
 ps -ef | grep  listenTronlinkException.sh | grep -v grep | awk '{print $2}' | xargs kill -9
 ps -ef | grep  "adb logcat" | grep -v grep | awk '{print $2}' | xargs kill -9
-rm -rf /Users/tron/Documents/tronlink_task/logs/exceptionLogs
-cp -rf exceptionLogs /Users/tron/Documents/tronlink_task/logs/exceptionLogs
+
+exceptionLogs_dir="exceptionLogs_`date +%Y%m%d%H%M%S`_dir"
+cp -rf exceptionLogs /Users/tron/Documents/ExceptionLogs/$exceptionLogs_dir
+find /Users/tron/Documents/ExceptionLogs/ -mtime +20 -name "*_*_dir" -exec rm -rf {} \;
 echo "Finish android.com.tronlink task"
