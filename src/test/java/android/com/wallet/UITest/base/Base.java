e8a46be771dfb77fdb6ea86a6fa4c0e1c4c59534
hechuan <hccgk@sina.com>
2022-08-17 13:57:21 +0800

    no message

--- src/test/java/android/com/wallet/UITest/base/Base.java
+++ src/test/java/android/com/wallet/UITest/base/Base.java
@@ -48,7 +48,7 @@ public class Base {
 
     private SimpleDateFormat timeStamp = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
-    public  int RetryAgainTimes = 3;
+    public  int RetryAgainTimes = 4;
 
     protected DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 
