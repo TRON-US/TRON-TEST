1f2bf0e933abc522ef63bb9617e2cecb1e46849e
hechuan <hccgk@sina.com>
2023-04-04 20:53:55 +0800

    update deposit1.0 to 2.0 and add 3case

--- src/test/java/ios/tronlink/com/tronlink/wallet/regression/FrozenAndUnfreezeTest.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/regression/FrozenAndUnfreezeTest.java
@@ -13,148 +13,87 @@ public class FrozenAndUnfreezeTest extends BaseTest {
 
     public FrozenAndUnfreezePage interferonPage() throws Exception{
         AssetPage asset = new AssetPage(DRIVER);
-        TimeUnit.SECONDS.sleep(3);
-        return asset.enterFrozenAndThawingPage();
+        FrozenAndUnfreezePage page = asset.enterFrozenAndThawingPage();
+        page.enterDepositPage();
+        return page;
+
     }
 
 
     @Test(description = "enter Details of the rules", alwaysRun = true)
     public void test001_enterDetailsOfTheRules() throws Exception {
-        FrozenAndUnfreezePage frozen = interferonPage();
+        AssetPage asset = new AssetPage(DRIVER);
+        FrozenAndUnfreezePage frozen = asset.enterFrozenAndThawingPage();
         DetailsAndRulesPage detailsAndRules = frozen.enterDetailsAndRulesPage();
         TimeUnit.SECONDS.sleep(2);
-        Assert.assertTrue(isElementExist("质押解锁说明"));
-        Assert.assertTrue(isElementExist("1. 质押 TRX 可以选择获得带宽或者能量中的一种，同时可以获得投票权，质押 1 TRX 可以获得 1 个投票权。"));
+        Assert.assertTrue(isElementExist("质押机制已升级为质押 2.0"));
+        Assert.assertTrue(isElementExist("1. 质押已升级全新 2.0 版本，支持随时解锁 TRX ；"));
+        Assert.assertTrue(isElementExist("了解更多"));
         Assert.assertTrue(isElementExist("我知道了"));
         detailsAndRules.iKnowButton.click();
         Assert.assertFalse(isElementExist("我知道了"));
     }
 
-    @Test(description = "Freeze energy detail", alwaysRun = true)
-    public void test002_3_FreezeEnergyDetail() throws Exception{
+    @Test(description = "FreezePageDetail", alwaysRun = true)
+    public void test002_FreezePageDetail() throws Exception{
         AssetPage asset = new AssetPage(DRIVER);
         FrozenAndUnfreezePage page = asset.enterFrozenAndThawingPage();
         TimeUnit.SECONDS.sleep(1);
-        Assert.assertTrue(isElementExist("预计可得"));
-        Assert.assertTrue(isElementExist("下一步"));
-        Assert.assertTrue(isElementExist("多重签名质押"));
-        Assert.assertTrue(isElementExist("质押 TRX  (1/2)"));
-        page.bandWidthTab().click();
-        Assert.assertTrue(isElementExist("预计可得"));
-        Assert.assertTrue(isElementExist("下一步"));
-        Assert.assertTrue(isElementExist("多重签名质押"));
-        Assert.assertTrue(isElementExist("质押 TRX  (1/2)"));
+        Assert.assertTrue(isElementExist("多重签名"));
+        Assert.assertTrue(isElementExist("了解质押 2.0"));
+        page.enterDepositPage();
+        Assert.assertTrue(isElementExist("StakeV2 WillGet Tip"));
+        Assert.assertTrue(isElementExist("* 仅支持给自己质押，质押获得的资源可随时代理给他人"));
+        Assert.assertTrue(isElementExist("质押"));
+        page.willGetTip().click();
+        Assert.assertTrue(isElementExist("质押 2.0 说明"));
     }
 
+    @Test(description = "Freeze Energy", alwaysRun = true)
+    public void test003_freezeEnergy() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        FrozenAndUnfreezePage page = asset.enterFrozenAndThawingPage();
+        page.enterDepositPage();
+        page.inputFrozenCount("1");
+        Assert.assertTrue(page.getFreeze_btn().isEnabled());
+        page.getFreeze_btn().click();
+        page.unTillSomeThing("确认交易");
+        Assert.assertTrue(isElementExist("质押 2.0"));
+        page.confirmAndInputPassword();
+        page.unTillSomeThing("完成");
+        Assert.assertTrue(isElementExist("质押成功"));
+        Assert.assertTrue(isElementExist("去投票获取收益"));
+    }
 
-    //Freeze Energy more than trx
-//    @Test(description = "Freeze Energy more than trx", alwaysRun = true)
-//    public void test004_freezeEnergyMoreThanTrx() throws Exception {
-//        FrozenAndUnfreezePage frozen = interferonPage();
-//        frozen.inputFrozenCount("99999999");
-//        Assert.assertTrue(isElementExist(" TRX 不足"));
-//        Assert.assertFalse(frozen.getFreeze_btn().isEnabled());
-//
-//    }
-//    //Freeze Energy with 0 trx
-//    @Test(description = "Freeze Energy with zero trx", alwaysRun = true)
-//    public void test005_freezeEnergyZeroTrx() throws Exception {
-//        FrozenAndUnfreezePage frozen = interferonPage();
-//        frozen.inputFrozenCount("0");
-//        Assert.assertTrue(isElementExist(" 最小质押数量为 1 TRX"));
-//        Assert.assertFalse(frozen.getFreeze_btn().isEnabled());
-//    }
-//
-//    //Freeze Energy equals trx
-//    @Test(description = "Freeze Energy equals trx", alwaysRun = true)
-//    public void test006_freeze100EqualTrx() throws Exception {
-//        FrozenAndUnfreezePage frozen = interferonPage();
-//        frozen.click100();
-//        Assert.assertTrue(frozen.getFreeze_btn().isEnabled());
-//        //TODO: 能量变化，投票权变化
-//    }
+    @Test(description = "test004_freezeBandWidth", alwaysRun = true)
+    public void test004_freezeBandWidth() throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        FrozenAndUnfreezePage page = asset.enterFrozenAndThawingPage();
+        page.enterDepositPage();
+        page.tv_stake_bandwidth.click();
+        page.inputFrozenCount("1");
+        Assert.assertTrue(page.getFreeze_btn().isEnabled());
+        page.getFreeze_btn().click();
+        page.unTillSomeThing("确认交易");
+        Assert.assertTrue(isElementExist("质押 2.0"));
+        page.confirmAndInputPassword();
+        page.unTillSomeThing("完成");
+        Assert.assertTrue(isElementExist("质押成功"));
+        Assert.assertTrue(isElementExist("去投票获取收益"));
+    }
 
     //Freeze Energy with null trx
     @Test(description = "Freeze Energy with zero trx", alwaysRun = true)
-    public void test007_freezeEnergyNullTrx() throws Exception {
+    public void test005_freezeEnergyNullTrx() throws Exception {
         FrozenAndUnfreezePage frozen = interferonPage();
         frozen.inputFrozenCount("0");
         Assert.assertTrue(isElementExist(" 最小质押数量为 1 TRX"));
         Assert.assertFalse(frozen.getFreeze_btn().isEnabled());
     }
 
-    @Test(description = "freeze Energy with Error Receiving Address", alwaysRun = true)
-    public void test008_freezeEnergyReceivingErrorAddress() throws Exception {
-        FrozenAndUnfreezePage frozen = interferonPage();
-        frozen.inputFrozenCount("1");
-        frozen.getFreeze_btn().click();
-        frozen.inputReceivingAddress("TG5wFVvrJiTkBA1WaZN3pzyJDfkgHMn");
-        Assert.assertTrue(isElementExist(" 账户地址格式不正确，账户地址须以 T 开头，长度为 34 个字符"));
-        Assert.assertFalse(frozen.confirmDeposit().isEnabled());
-    }
 
-    @Test(description = "freeze Energy with not active Receiving Address", alwaysRun = true)
-    public void test009_freezeEnergyNotActiveReceivingAddress() throws Exception {
-        FrozenAndUnfreezePage frozen = interferonPage();
-        frozen.inputFrozenCount("1");
-        frozen.getFreeze_btn().click();
-        frozen.inputReceivingAddress("TWRjSKWxoDMetK4dhFeM763zGJZqu5oBxQ");
-        Assert.assertTrue(isElementExist(" 该账户未在波场网络上激活，请重新填写"));
-        Assert.assertFalse(frozen.confirmDeposit().isEnabled());
-    }
-
-//    /**
-//     freeze Energy
-//     */
-//    @Test(groups = {"P0"},description = "Freeze Energy Scuuess", alwaysRun = true)
-//    public void test010_freezeEnergySuccess() throws Exception {
-//        FrozenAndUnfreezePage frozen = interferonPage();
-//        frozen.inputFrozenCount("1");
-//        frozen.getFreeze_btn().click();
-//        TimeUnit.SECONDS.sleep(1);
-//        Assert.assertFalse(frozen.confirmDeposit().isEnabled());
-//        frozen.agreeClick();
-//        TimeUnit.SECONDS.sleep(1);
-//        Assert.assertTrue(frozen.confirmDeposit().isEnabled());
-//        frozen.confirmDeposit().click();
-//        TimeUnit.SECONDS.sleep(3);
-//        Assert.assertTrue(isElementExist("1 票"));
-//        frozen.confirm_btn().click();
-//        frozen.frozenInputPassword();
-//        Assert.assertTrue(isElementExist("质押成功"));
-//        Assert.assertTrue(isElementExist("去投票获取收益"));
-//        Assert.assertTrue(isElementExist("1"));
-//        Assert.assertTrue(frozen.getFinish_btn().isEnabled());
-//        frozen.getFinish_btn().click();
-//        Assert.assertTrue(isElementExist("我的资源"));
-//    }
-//
-//    @Test(groups = {"P0"},description = "Freeze Energy Scuuess", alwaysRun = true)
-//    public void test011_freezeBandWidthSuccess() throws Exception {
-//        FrozenAndUnfreezePage frozen = interferonPage();
-//        frozen.bandWidthTab().click();
-//        frozen.inputFrozenCount("1");
-//        frozen.getFreeze_btn().click();
-//        TimeUnit.SECONDS.sleep(1);
-//        Assert.assertFalse(frozen.confirmDeposit().isEnabled());
-//        frozen.agreeClick();
-//        TimeUnit.SECONDS.sleep(1);
-//        Assert.assertTrue(frozen.confirmDeposit().isEnabled());
-//        frozen.confirmDeposit().click();
-//        TimeUnit.SECONDS.sleep(3);
-//        Assert.assertTrue(isElementExist("1 票"));
-//        frozen.confirm_btn().click();
-//        frozen.frozenInputPassword();
-//        Assert.assertTrue(isElementExist("质押成功"));
-//        Assert.assertTrue(isElementExist("去投票获取收益"));
-//        Assert.assertTrue(isElementExist("1"));
-//        Assert.assertTrue(frozen.getFinish_btn().isEnabled());
-//        frozen.getFinish_btn().click();
-//        Assert.assertTrue(isElementExist("我的资源"));
-//    }
-/////
     @Test(groups = {"P0"},alwaysRun = true)
-    public void test012_inputPercentTest() throws Exception {
+    public void test006_inputPercentTest() throws Exception {
         FrozenAndUnfreezePage frozen = interferonPage();
         Double total = sepLeftNumberTextToDouble(frozen.tv_available_amount.getText().replace("可用: ",""),"TRX");
         frozen.amount_percent_25.click();
@@ -173,7 +112,7 @@ public class FrozenAndUnfreezeTest extends BaseTest {
     }
 
     @Test(alwaysRun = true)
-    public void test013_inputEnergyAndBandWidthCountTest() throws Exception {
+    public void test007_inputEnergyAndBandWidthCountTest() throws Exception {
         FrozenAndUnfreezePage frozen = interferonPage();
         frozen.selectEnergyTab();
         frozen.inputAmount("80");
@@ -185,25 +124,51 @@ public class FrozenAndUnfreezeTest extends BaseTest {
         Assert.assertTrue(isElementExist("80"));
     }
 
-
-//    @Test(alwaysRun = true)
-//    public void test014_inputEnergyTRXCountTest() throws Exception {
-//        FrozenAndUnfreezePage frozen = interferonPage();
-//        frozen.selectEnergyTab();
-//        frozen.slideScreenBottom();
-//        frozen.enterEnergyBandWidth("20000");
-//        Assert.assertEquals(frozen.et_amount.getText(),"74");
-//        Assert.assertTrue(isElementExist("74"));
-//    }
-
     @Test(alwaysRun = true)
-    public void test015_inputBandWidthTRXCountTest() throws Exception {
+    public void test008_inputBandWidthTRXCountTest() throws Exception {
         FrozenAndUnfreezePage frozen = interferonPage();
         frozen.selectBandWidthTab();
         frozen.slideScreenBottom();
-        frozen.enterEnergyBandWidth("345");
+        frozen.enterEnergyBandWidth("340");
         Assert.assertEquals(frozen.et_amount.getText(),"70");
         Assert.assertTrue(isElementExist("70"));
     }
 
+    @Test(alwaysRun = true)
+    public void test009_unFreezeTest() throws Exception{
+        AssetPage asset = new AssetPage(DRIVER);
+        FrozenAndUnfreezePage page = asset.enterFrozenAndThawingPage();
+        page.enterUnFreezePage();
+        if(isElementExist("选择您要解锁的 TRX ")){
+            page.getUnFreeze20().click();
+        }
+        Assert.assertTrue(isElementExist("* 解除 TRX 需要 1 天的等待期，解除质押的资产在 1 天后可取出"));
+        if(isElementExist("0 TRX")){
+            Assert.assertTrue(isElementExist("输入解锁数量"));
+        }else {
+            page.inputText("1");
+            page.unFreezeAndInputPassword();
+            Assert.assertTrue(isElementExist("解锁成功"));
+        }
+    }
+
+    @Test(alwaysRun = true)
+    public void test010_enterVotePageTest()throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        FrozenAndUnfreezePage page = asset.enterFrozenAndThawingPage();
+        page.enterVotePage();
+        TimeUnit.SECONDS.sleep(2);
+        Assert.assertTrue(isElementExist("投票"));
+        Assert.assertTrue(isElementExist("剩余投票权"));
+        Assert.assertTrue(isElementExist("超级代表"));
+    }
+
+    @Test(alwaysRun = true)
+    public void test011_enterResourcePageTest()throws Exception {
+        AssetPage asset = new AssetPage(DRIVER);
+        FrozenAndUnfreezePage page = asset.enterFrozenAndThawingPage();
+        page.enterResourcePage();
+        Assert.assertTrue(isElementExist("资源管理"));
+        Assert.assertTrue(isElementExist("多重签名"));
+    }
 }
