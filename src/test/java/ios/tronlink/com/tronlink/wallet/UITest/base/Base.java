5563fd2f0bae12e976cb5d61974007f104341b0f
hechuan <hccgk@sina.com>
2023-04-10 17:09:13 +0800

    time

--- src/test/java/ios/tronlink/com/tronlink/wallet/UITest/base/Base.java
+++ src/test/java/ios/tronlink/com/tronlink/wallet/UITest/base/Base.java
@@ -42,7 +42,7 @@ public class Base {
 
     private SimpleDateFormat timeStamp = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss ");
 
-    public int RetryAgainTimes = 0;
+    public int RetryAgainTimes = 2;
 
     protected DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 
